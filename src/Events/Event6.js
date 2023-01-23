9ffe3c560991d46e5691028faba8eaf15e2624ed
Your name <teak.yuktanand@gmail.com>
2023-01-23 11:30:32 +0700

    feat: add Event6

---
+++ src/Events/Event6.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Event6 = () => {
+  return (
+    <div>
+      <input
+        className=" my-2 border border-red-700 rounded-lg"
+        onSelect={() => console.log("Hello 6")}
+        placeholder="Hello 6"
+      />
+    </div>
+  );
+};
+export default Event6;
