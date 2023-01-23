9b31a7f00910f2c87f3fdfc806e07a780a1acafa
Your name <teak.yuktanand@gmail.com>
2023-01-23 15:44:49 +0700

    State2 update

--- src/States/State2.js
+++ src/States/State2.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const State2 = () => {
   const [counter, setCounter] = useState(0);
   return (
-    <div className="bg-blue-200 p-2 m-2">
+    <div className="bg-blue-200 p-2 m-2 text-green-700">
       <h1>{counter}</h1>
       <button
         className="bg-red-200 rounded-lg p-2 m-2"
