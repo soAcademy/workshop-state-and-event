920fe949cec7e0a1728288bcd9c20c2eda5326b7
korana <korayut@gmail.com>
2023-01-23 10:55:45 +0700

    feat: add Event4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Event1 from "./Events/Event1.js";
 import Event2 from "./Events/Event2.js";
 import Event3 from "./Events/Event3.js";
+import Event4 from "./Events/Event4.js";
 
 const App = () => {
   return (
     <div>
       {/* <Event1 /> */}
       {/* <Event2 /> */}
-      <Event3 />
+      {/* <Event3 /> */}
+      <Event4 />
     </div>
   );
 };
---
+++ src/Events/Event4.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Event4 = () => {
+  const inputChange = (e) => console.log(e.target.value);
+  return (
+    <div className="my-2">
+      <input
+        className="border-2 border-red-200"
+        onChange={(e) => inputChange(e)}
+      />
+    </div>
+  );
+};
+
+export default Event4;
