765ab25fcc1c11dd2ff6aa83925774f41d7ff757
korana <korayut@gmail.com>
2023-01-23 11:24:49 +0700

    feat: add Event6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Event2 from "./Events/Event2.js";
 import Event3 from "./Events/Event3.js";
 import Event4 from "./Events/Event4.js";
 import Event5 from "./Events/Event5.js";
+import Event6 from "./Events/Event6.js";
 
 const App = () => {
   return (
@@ -11,7 +12,8 @@ const App = () => {
       {/* <Event2 /> */}
       {/* <Event3 /> */}
       {/* <Event4 /> */}
-      <Event5 />
+      {/* <Event5 /> */}
+      <Event6 />
     </div>
   );
 };
--- src/Events/Event5.js
+++ src/Events/Event5.js
@@ -3,7 +3,8 @@ import React from "react";
 const Event5 = () => {
     return (
         <div className="my-2 bg-blue-200 h-64"
-        onMouseOver={() => console.log("Hello 5")}>
+        onMouseOver={() => console.log("Hello 5")}
+        onMouseLeave={() => console.log("Exit 5")}>
         Hover me
         </div>
     )
