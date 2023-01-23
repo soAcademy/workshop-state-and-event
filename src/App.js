a93ae8f44ce8a1f64ccd413d66711ef1278469ec
korana <korayut@gmail.com>
2023-01-23 10:40:06 +0700

    feat: add Event3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Event1 from "./Events/Event1.js";
 import Event2 from "./Events/Event2.js";
+import Event3 from "./Events/Event3.js";
 
 const App = () => {
   return (
     <div>
       {/* <Event1 /> */}
-      <Event2 />
+      {/* <Event2 /> */}
+      <Event3 />
     </div>
   );
 };
