11e765c3c914f2605a9196b5af83de2451fc6478
Phisit Siprasatthong <phisite@gmail.com>
2023-01-23 14:44:15 +0700

    feat: add solution for State4

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@
 
 // import State1 from "./States/State1";
 // import State2 from "./States/State2";
-import State3 from "./States/State3";
-// import State4 from "./States/State4";
+// import State3 from "./States/State3";
+import State4 from "./States/State4";
 // import State5 from "./States/State5";
 
 const App = () => {
@@ -56,8 +56,8 @@ const App = () => {
 
       {/* <State1 /> */}
       {/* <State2 /> */}
-      <State3 />
-      {/* <State4 /> */}
+      {/* <State3 /> */}
+      <State4 />
       {/* <State5 /> */}
       {/* <State6 /> */}
 
