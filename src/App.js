5d5eeb71906bb85bcda9fc1d272b71f319c946ea
Phisit Siprasatthong <phisite@gmail.com>
2023-01-23 10:12:46 +0700

    feat: add solution for Event1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import Event1 from "./EventsAnswer/Event1";
-import Event2 from "./EventsAnswer/Event2";
-import Event3 from "./EventsAnswer/Event3";
-import Event4 from "./EventsAnswer/Event4";
-import Event5 from "./EventsAnswer/Event5";
-import Event6 from "./EventsAnswer/Event6";
-import Event7 from "./EventsAnswer/Event7";
-import Event8 from "./EventsAnswer/Event8";
+// import Event1 from "./EventsAnswer/Event1";
+// import Event2 from "./EventsAnswer/Event2";
+// import Event3 from "./EventsAnswer/Event3";
+// import Event4 from "./EventsAnswer/Event4";
+// import Event5 from "./EventsAnswer/Event5";
+// import Event6 from "./EventsAnswer/Event6";
+// import Event7 from "./EventsAnswer/Event7";
+// import Event8 from "./EventsAnswer/Event8";
 
-import State1 from "./StatesAnswer/State1";
-import State2 from "./StatesAnswer/State2";
-import State3 from "./StatesAnswer/State3";
-import State4 from "./StatesAnswer/State4";
-import State5 from "./StatesAnswer/State5";
-import State6 from "./StatesAnswer/State6";
+// import State1 from "./StatesAnswer/State1";
+// import State2 from "./StatesAnswer/State2";
+// import State3 from "./StatesAnswer/State3";
+// import State4 from "./StatesAnswer/State4";
+// import State5 from "./StatesAnswer/State5";
+// import State6 from "./StatesAnswer/State6";
 
-import Prop1 from "./PropsAnswer/Prop1";
-import Prop2 from "./PropsAnswer/Prop2";
-import Prop3 from "./PropsAnswer/Prop3";
-import Prop4 from "./PropsAnswer/Prop4";
+// import Prop1 from "./PropsAnswer/Prop1";
+// import Prop2 from "./PropsAnswer/Prop2";
+// import Prop3 from "./PropsAnswer/Prop3";
+// import Prop4 from "./PropsAnswer/Prop4";
 
-import Effect1 from "./EffectsAnswer/Effect1";
-import Effect2 from "./EffectsAnswer/Effect2";
-import Effect3 from "./EffectsAnswer/Effect3";
-import Effect4 from "./EffectsAnswer/Effect4";
-import Effect5 from "./EffectsAnswer/Effect5";
-import Effect6 from "./EffectsAnswer/Effect6";
-import Effect7 from "./EffectsAnswer/Effect7";
+// import Effect1 from "./EffectsAnswer/Effect1";
+// import Effect2 from "./EffectsAnswer/Effect2";
+// import Effect3 from "./EffectsAnswer/Effect3";
+// import Effect4 from "./EffectsAnswer/Effect4";
+// import Effect5 from "./EffectsAnswer/Effect5";
+// import Effect6 from "./EffectsAnswer/Effect6";
+// import Effect7 from "./EffectsAnswer/Effect7";
+
+import Event1 from "./Events/Event1";
 
 const App = () => {
   return (
     <div>
-      {/* <Event1 /> */}
+      <Event1 />
       {/* <Event2 /> */}
       {/* <Event3 /> */}
       {/* <Event4 /> */}
---
+++ src/Events/Event1.js
@@ -0,0 +1,4 @@
+const Event1 = () => (
+  <button onClick={() => console.log("Hello 1")}>Button 1</button>
+);
+export default Event1;
