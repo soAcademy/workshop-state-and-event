1e5d759a59a9f193335769e23083e8686c7b0e31
Bond Rungrot <st122150@ait.asia>
2023-01-23 11:14:32 +0700

    init

--- src/Events/Event5.js
+++ src/Events/Event5.js
@@ -2,7 +2,7 @@ const Event5 = () => {
   return (
     <div className="my-2">
       <div
-        className="bg-white-600 text-bold"
+        className="bg-gray-600 text-primary text-bold-center"
         onMouseOver={() => console.log("Hello 5")}
       >Button 5</div>
     </div>
@@ -13,4 +13,4 @@ export default Event5;
 
 
 // 5. สร้าง div ที่วาง Mouse แล้ว console.log("Hello 5"); (onMouseOver)
-//เหมืนอข้อหนึ่งเปะ ถ้าเราใช้ตัวนี้มันจะทำให้เราเหมือนได้ยอดคลิกมาเยอะขึ้นมาก เฉียบไปเลย 
+//เหมืนอข้อหนึ่งเปะ ถ้าเราใช้ตัวนี้มันจะทำให้เราเหมือนได้ยอดคลิกมาเยอะขึ้นมาก เฉียบไปเลย  
