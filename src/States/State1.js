9cb7eb109eba819b5f5eccba670229658494cf0c
jaiieth <midzty.rb128@gmail.com>
2023-01-23 15:25:33 +0700

    fix:remove console.log

--- src/States/State1.js
+++ src/States/State1.js
@@ -4,7 +4,6 @@ const State1 = () => {
   const [state, setState] = useState("Bin");
   const click = () => {
     state === "Bin" ? setState("Jam") : setState("Bin");
-    console.log({ state });
   };
   return (
     <div>
