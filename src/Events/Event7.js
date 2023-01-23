9b3ce256572ab0064f9b50b0c36b0308b3ba41f4
Phisit Siprasatthong <phisite@gmail.com>
2023-01-23 11:55:48 +0700

    feat: add solution for Event7

---
+++ src/Events/Event7.js
@@ -0,0 +1,35 @@
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const formValue = {
+    name: e.target[0].value,
+    tel: e.target[1].value,
+    email: e.target[2].value,
+  };
+  console.log(formValue);
+};
+
+const Event7 = () => {
+  return (
+    <form action="" onSubmit={(e) => onFormSubmit(e)}>
+      <input
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Name"
+        required="required"
+      />
+      <input
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Tel"
+      />
+      <input
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Email"
+      />
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Event7;
