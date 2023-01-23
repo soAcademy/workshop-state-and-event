dadf73dad3fed636d7d5196dfe41e0d1117891d5
Your name <your@email.com>
2023-01-23 10:42:03 +0700

    feat: add Event3

---
+++ src/Events/Event3.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Event3 = () => {
+  const helloWorld = () => console.log("Hello 3");
+  return (
+    <div className="my-2">
+      <button className="bg-red-200" onClick={helloWorld}>
+        Button 3
+      </button>
+    </div>
+  );
+};
+
+export default Event3;
