229f37e9f76e2b2279bf3230ec6fce6267a3f01b
Poom <poomhan@gmail.com>
2023-01-23 17:07:24 +0700

    state7

--- src/States/State7.js
+++ src/States/State7.js
@@ -2,12 +2,26 @@ import React, { useState } from "react";
 
 const State7 = () => {
   const [toggle, setToggle] = useState(false);
+  const [toggle1, setToggle1] = useState(false);
 
   return (
-    <div className="">
+    <div className="bg-red-500">
       <button className="bg-red-700" onClick={() => setToggle(!toggle)}>
         Toggle
       </button>
+      {toggle && (
+        <div className="bg-purple-700">
+       Show
+    
+            <button className="bg-red-700" onClick={() => setToggle1(!toggle1)}>
+              Toggle1
+            </button>
+            {toggle1 && <div className="bg-purple-700">Show1</div>}
+
+          
+        </div>
+      )}
+      {toggle && <div className="bg-purple-700">Show</div>}
       {toggle && <div className="bg-purple-700">Show</div>}
     </div>
   );
