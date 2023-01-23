b2be1a54260e4ffbe751d013cda9829e4ca8134a
Phisit Siprasatthong <phisite@gmail.com>
2023-01-23 14:54:12 +0700

    feat: add solution for State5

---
+++ src/States/State5.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const State1 = () => {
+  const initialCustomer = { name: "Bin", location: "Bangkok" };
+  const [customer, setCustomer] = useState(initialCustomer);
+  return (
+    <>
+      <div>{JSON.stringify(customer)}</div>
+      <button
+        type="button"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        onClick={() => setCustomer({ name: "Jam", location: "Nonthaburi" })}
+      >
+        Set Customer
+      </button>
+    </>
+  );
+};
+
+export default State1;
