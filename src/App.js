fedca71c0bc84b403df0ea128123ad737b9caf87
Phisit Siprasatthong <phisite@gmail.com>
2023-01-23 10:41:14 +0700

    feat: add solution for Event3

--- src/App.js
+++ src/App.js
@@ -28,15 +28,15 @@
 // import Effect7 from "./EffectsAnswer/Effect7";
 
 // import Event1 from "./Events/Event1";
-import Event2 from "./Events/Event2";
+// import Event2 from "./Events/Event2";
+import Event3 from "./Events/Event3";
 
 const App = () => {
   return (
     <div>
       {/* <Event1 /> */}
       {/* <Event2 /> */}
-      <Event2 />
-      {/* <Event3 /> */}
+      <Event3 />
       {/* <Event4 /> */}
       {/* <Event5 /> */}
       {/* <Event6 /> */}
---
+++ src/Events/Event3.js
@@ -0,0 +1,7 @@
+const helloWorld = () => {
+  console.log("Hello 3");
+};
+
+const Event2 = () => <button onClick={helloWorld}>Button 3</button>;
+
+export default Event2;
