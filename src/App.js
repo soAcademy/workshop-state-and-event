8e8df50c16ce12f28d2deb16ca199a23c9b10bfd
Poom <poomhan@gmail.com>
2023-01-23 14:09:39 +0700

    state2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import Event1 from "./Events/Event1";
-import Event2 from "./Events/Event2";
-import Event3 from "./Events/Event3";
-import Event4 from "./Events/Event4";
-import Event5 from "./Events/Event5";
-import Event6 from "./Events/Event6";
-import Event7 from "./Events/Event7";
-import Event8 from "./Events/Event8";
+// import Event1 from "./Events/Event1";
+// import Event2 from "./Events/Event2";
+// import Event3 from "./Events/Event3";
+// import Event4 from "./Events/Event4";
+// import Event5 from "./Events/Event5";
+// import Event6 from "./Events/Event6";
+// import Event7 from "./Events/Event7";
+// import Event8 from "./Events/Event8";
 
-import State1 from "./States/State1";
+// import State1 from "./States/State1";
+import State2 from "./States/State2";
 const App = () => {
   return (
     <div>
@@ -19,7 +20,8 @@ const App = () => {
       {/* <Event6 /> */}
       {/* <Event7 /> */}
       {/* <Event8 /> */}
-      <State1 />
+      {/* <State1 /> */}
+      <State2 />
     </div>
   );
 };
---
+++ src/States/State2.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const State2 = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-red-200">
+      <p>{count}</p>
+      <p>
+        <button className="p-4 bg-blue-200" onClick={() => setCount(count+1)}>
+          Change Count
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default State2;
