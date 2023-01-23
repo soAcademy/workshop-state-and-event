bdf11b7f9c84caad60046c6d5e73c11d82fe1e8b
BornDhamachart <dhamachart.c@gmail.com>
2023-01-23 11:25:18 +0700

    feat:submit workshop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 // import Event2 from "./Events/Event2";
 // import Event3 from "./Events/Event3";
 // import Event4 from "./Events/Event4";
-import Event5 from "./Events/Event5";
+// import Event5 from "./Events/Event5";
+import Event6 from "./Events/Event6";
 
 const App = () => {
   return (
@@ -11,7 +12,8 @@ const App = () => {
       {/* <Event2 /> */}
       {/* <Event3 /> */}
       {/* <Event4 /> */}
-      <Event5 />
+      {/* <Event5 /> */}
+      <Event6 />
     </div>
   );
 };
---
+++ src/Events/Event6.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Event6 = () => {
+  return (
+    <input
+      className="bg-yellow-200 border border-blue"
+      onSelect={() => console.log("Hello 6")}
+      placeholder="Hello"
+    />
+  );
+};
+
+export default Event6;
