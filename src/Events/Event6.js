2f9775d4e26cfcb5e4fb18d514390966cf635e73
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-01-23 11:27:31 +0700

    event6

--- src/Events/Event6.js
+++ src/Events/Event6.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Event6=()=>{
   
   return(
-    <input></input>
+    <input className='border-2px border-cyan-500 w-[150px] my-2 ml-5' onSelect={()=>console.log('hello 555')} placeholder='Fill the form'></input>
   );
 };
 export default Event6;
+//state activate 2 times
+//first click
+//second fill info.
