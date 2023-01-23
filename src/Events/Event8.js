5383ec9edc64aa9a06d530008aa6d41d30ee3a7f
jaiieth <midzty.rb128@gmail.com>
2023-01-23 13:29:00 +0700

    feat: Event8 again

--- src/Events/Event8.js
+++ src/Events/Event8.js
@@ -1,9 +1,14 @@
 const Event8 = () => {
   return (
-    <label>Event8 - // Image Upload Button // : &nbsp;
-    <input type="file" accept="image/*" onChange={(e)=> console.log(e.target.files[0].name)}></input>
-    </label>
-  )
-}
-
-export default Event8
+    <div className="bg-blue-300 m-4 p-4 rounded-lg w-fit flex flex-col justify-center items-center">
+      <p>Event8 - Image Upload Button </p>
+      <input
+        type="file"
+        accept="image/jpeg, image/png"
+        onChange={(e) => console.log(e.target.files[0].name)}
+        className="bg-gray-100"
+      />
+    </div>
+  );
+};
+export default Event8;
