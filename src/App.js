0d00ee50c28f61545ab773fa1e280737d58fc7dd
mplug <oppoalien@gmail.com>
2023-01-23 10:23:26 +0700

    feat: do the workshop2 state and event

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Event1 from "./Events/Event1";
-// import Event2 from "./Events/Event2";
+// import Event1 from "./Events/Event1";
+import Event2 from "./Events/Event2";
 // import Event3 from "./Events/Event3";
 // import Event4 from "./Events/Event4";
 // import Event5 from "./Events/Event5";
@@ -30,8 +30,8 @@ import Event1 from "./Events/Event1";
 const App = () => {
   return (
     <div>
-      <Event1 />
-      {/* <Event2 /> */}
+      {/* <Event1 /> */}
+      <Event2 />
       {/* <Event3 /> */}
       {/* <Event4 /> */}
       {/* <Event5 /> */}
--- src/Events/Event2.js
+++ src/Events/Event2.js
@@ -1,14 +1,14 @@
 const Event2 = () => {
   const helloWorld = () => {
-    console.log('Hello2');
-  }
+    console.log("Hello2");
+  };
 
   return (
     <div className="m-2">
       <button
         type="button"
         className="bg-red-200 rounded-lg shadow-md p-2"
-        onClick={() => helloWorld() }
+        onClick={() => helloWorld()}
       >
         Button2
       </button>
