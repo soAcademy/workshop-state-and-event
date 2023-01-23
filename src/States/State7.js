767a766eb99d56811e2331b3d59b1fe94a2b0802
kunlanis <yoo.kunlanis@gmail.com>
2023-01-23 15:51:25 +0700

    feat:toggle menu

---
+++ src/States/State7.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const State7 = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="">
+      <button
+        className=" p-2 bg-red-200 rounded-lg"
+        onClick={() => setToggle(!toggle)}
+      >
+        Menu
+      </button>
+      {toggle && (
+        <div className="rounded-lg ml-3 py-1 bg-purple-200 w-1/6">
+          <ul>
+            <li className="rounded-lg bg-purple-300 p-1 mx-1 mb-1"><a href="#">item 1</a></li>
+            <li className="rounded-lg bg-purple-300 p-1 mx-1 mb-1"><a href="#">item 2</a></li>
+            <li className="rounded-lg bg-purple-300 p-1 mx-1 mb-1"><a href="#">item 3</a></li>
+            <li className="rounded-lg bg-purple-300 p-1 mx-1"><a href="#">item 4</a></li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default State7;
