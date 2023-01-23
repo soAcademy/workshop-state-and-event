95b7a29ac1de5b3d15c2961deb4c53f33b9106de
Bond Rungrot <st122150@ait.asia>
2023-01-23 10:25:19 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Event1 from './Events/Event1';
-
+import Event2 from './Events/Event2';
 
 
 const App = () => {
   return (
     <div>
       <Event1 />
-      {/* <Event2 /> */}
+      <Event2 />
       {/* <Event3 /> */}
       {/* <Event4 /> */}
       {/* <Event5 /> */}
---
+++ src/Events/Event2.js
@@ -0,0 +1,12 @@
+const Event2 = () => {
+  return (
+    <div className="my-2">
+      <button
+        className="bg-gray-600 text-primary text-bold-center"
+        onClick={() => console.log("Hello World2")}
+      >Button 2</button>
+    </div>
+  );
+};
+
+export default Event2;
