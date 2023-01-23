e4c79f6d552312d5f74b1ca4ee7c3f352a2ce97b
Firstweera <firstweera357@gmail.com>
2023-01-23 11:57:41 +0700

    feat: Event7

--- src/Events/Event6.js
+++ src/Events/Event6.js
@@ -4,7 +4,7 @@ const Event6 = () => {
   return (
     <input
       type="text"
-      className="border-2 border-indigo-400 rounded-lg p-1 m-3"
+      className="border-2 border-indigo-400 rounded-lg p-1 m-3 text-center"
       onSelect={() => console.log("Hello 6")}
       placeholder="Hello"
     />
---
+++ src/Events/Event7.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Event7 = () => {
+  const submitData = (e) => {
+    e.preventDefault();
+    const data = {
+      name: e.target[0].value,
+      tel: e.target[1].value,
+      email: e.target[2].value,
+    };
+    console.log(data);
+  };
+
+  return (
+    <form className="m-3 bg-violet-400" onSubmit={(e) => submitData(e)}>
+      <div>
+        <input
+          className="h-12 border-2 border-red-400 m-2"
+          placeholder="Name"
+          id="name"
+          required
+        />
+      </div>
+      <div>
+        <input
+          className="h-12 border border-green-400 m-2"
+          placeholder="Telephone Number"
+          id="tel"
+        />
+      </div>
+      <div>
+        <input
+          className="h-12 border border-blue-400 m-2"
+          placeholder="Email Address"
+          id="email"
+        />
+      </div>
+      <button className="h-12 bg-black text-white p-4 m-2">Submit</button>
+      {/* <button className="w-[90px] h-12 bg-black text-white p-4">Clear</button> */}
+    </form>
+  );
+};
+
+export default Event7;
