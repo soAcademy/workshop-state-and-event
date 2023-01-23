aaf7eed56e128adfd7f79cc3cc947d3fedfc6625
Phisit Siprasatthong <phisite@gmail.com>
2023-01-23 15:52:53 +0700

    fix: replace class prop with className

--- src/Events/Event8.js
+++ src/Events/Event8.js
@@ -4,7 +4,7 @@ const onInputFile = (e) => {
 
 const Event8 = () => (
   <input
-    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+    className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
     id="file_input"
     type="file"
     accept="image/png image/jpeg"
--- src/States/State6.js
+++ src/States/State6.js
@@ -34,7 +34,7 @@ const State1 = () => {
       <div>Number 1 + Number 2: {sum}</div>
       <button
         type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         onClick={() => setSum(Number(number1) + Number(number2))}
       >
         Calculate
