f418677dc42c2aa6fb524cb874aa7404935143d0
Your name <teak.yuktanand@gmail.com>
2023-01-23 14:04:34 +0700

    feat: add State2

---
+++ src/States/State2.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const State2 = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="bg-blue-200 p-2 m-2">
+      <h1>{counter}</h1>
+      <button
+        className="bg-red-200 rounded-lg p-2 m-2"
+        onClick={() => setCounter(counter + 1)}
+      >
+        Counter
+      </button>
+    </div>
+  );
+};
+
+export default State2;
