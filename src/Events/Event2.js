908689fe4be701adf93201e5b44b04aacd3702db
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-23 10:28:38 +0700

    feat: even workshop 2

--- src/Events/Event2.js
+++ src/Events/Event2.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Event2 = () => {
-  const helloWorld = () => console.log("Hello 2");
-
+function Event2() {
+  const Event2 = () => {
+    const helloWorld = () => console.log("Hello 222+");
+  };
   return (
     <div className="my-2">
       <button className="bg-yellow-200" onClick={() => helloWorld()}>
@@ -10,6 +11,6 @@ const Event2 = () => {
       </button>
     </div>
   );
-};
+}
 
 export default Event2;
