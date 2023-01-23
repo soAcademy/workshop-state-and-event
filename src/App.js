b0ba575fcc8ece6dc4d001e8cc47b21c18c59d55
BornDhamachart <dhamachart.c@gmail.com>
2023-01-23 10:27:34 +0700

    feat:submit workshop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Event1 from "./Events/Event1";
+// import Event1 from "./Events/Event1";
+import Event2 from "./Events/Event2";
 
 const App = () => {
   return (
     <div>
-      <Event1 />
+      {/* <Event1 /> */}
+      <Event2 />
     </div>
   );
 };
---
+++ src/Events/Event2.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Event2 = () => {
+  const helloWorld = () => console.log("Hello 2");
+  return (
+    <div className="my-2">
+      <button className="bg-yellow-200" onClick={() => helloWorld()}>
+        Button 2
+      </button>
+    </div>
+  );
+};
+
+export default Event2;
