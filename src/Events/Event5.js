b460d142e3efb4f6c40cfd404591273d79c5d10e
Your name <teak.yuktanand@gmail.com>
2023-01-23 13:04:22 +0700

    fix:hover

--- src/Events/Event5.js
+++ src/Events/Event5.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Event5 = () => {
   return (
-    <div className="my-2 mx-12 bg-red-300 h-32" onMouseOver={() => console.log("Hello 5")}>
+    <div className=" flex my-2 mx-2 bg-red-300 h-32 rounded-full" onMouseOver={() => console.log("Hello 5")}>
+      <div className="mx-auto">
       Hover 5
+      </div>
     </div>
   );
 };
