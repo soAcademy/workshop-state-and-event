a7a00479a80911cf61524267ab6e4940d6af5202
kunlanis <yoo.kunlanis@gmail.com>
2023-01-23 10:07:07 +0700

    feat:buttons event1

---
+++ src/Events/Event1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Event1 = () => {
+  return (
+    <div className="ml-2">
+      <button className="bg-green-200" onClick={()=>console.log("Hello world!")}>Button1</button>
+    </div>
+  )
+}
+
+export default Event1;
