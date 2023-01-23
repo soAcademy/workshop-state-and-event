b6dc4bc328c44a0e856df62e1bdf701411446925
BornDhamachart <dhamachart.c@gmail.com>
2023-01-23 10:57:11 +0700

    feat:submit workshop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import Event1 from "./Events/Event1";
 // import Event2 from "./Events/Event2";
-import Event3 from "./Events/Event3";
+// import Event3 from "./Events/Event3";
+import Event4 from "./Events/Event4";
 
 const App = () => {
   return (
     <div>
       {/* <Event1 /> */}
       {/* <Event2 /> */}
-      <Event3 />
+      {/* <Event3 /> */}
+      <Event4 />
     </div>
   );
 };
