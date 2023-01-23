7503a36ffd180bd82250b0bf10418461b8c78216
Bond Rungrot <st122150@ait.asia>
2023-01-23 11:33:44 +0700

    init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Event2 from './Events/Event2';
 import Event3 from './Events/Event3';
 import Event4 from './Events/Event4';
 import Event5 from './Events/Event5';
+import Event6 from './Events/Event6';
 
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
       <Event3 />
       <Event4 />
       <Event5 />
-      {/* <Event6 /> */}
+      <Event6 />
       {/* <Event7 /> */}
       {/* <Event8 /> */}
 
---
+++ src/Events/Event6.js
@@ -0,0 +1,20 @@
+const event6 = () => {
+  return (
+    <input
+      className="my-2 bg-yellow-200 border border-black"
+      onSelect={() => console.log("Hello 6")}
+      placeholder="Hello"
+    />
+  );
+};
+
+export default event6;
+
+
+
+
+
+
+
+// 6. สร้าง input ที่มี placeholder ว่า "Hello" เมื่อคลิ้กที่จะ console.log("Hello 6") (onSelect)
+// onSelect มันจะแบบกดหนังทีปล่อย Event ปล่อยอีกทีก็เป็นอีก Event เราจะใช้ทำฟอร์มที่ไว้โชว์คำอธิบายว่าจะไว้กรอกอะไรยังไง
