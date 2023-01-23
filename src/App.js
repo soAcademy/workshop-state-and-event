7ab80b8cd786b233ba86482701e419496a0002c2
kunlanis <yoo.kunlanis@gmail.com>
2023-01-23 11:25:06 +0700

    feat:onSelect Event6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Event2 from "./Events/Event2";
 import Event3 from "./Events/Event3";
 import Event4 from "./Events/Event4";
 import Event5 from "./Events/Event5";
-// import Event6 from "./EventsAnswer/Event6";
+import Event6 from "./Events/Event6";
 // import Event7 from "./EventsAnswer/Event7";
 // import Event8 from "./EventsAnswer/Event8";
 
@@ -34,8 +34,8 @@ const App = () => {
       {/* <Event2 /> */}
       {/* <Event3 /> */}
       {/* <Event4 /> */}
-      <Event5 />
-      {/* <Event6 /> */}
+      {/* <Event5 /> */}
+      <Event6 />
       {/* <Event7 /> */}
       {/* <Event8 /> */}
 
---
+++ src/Events/Event6.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Event6 = () => {
+  return (
+    <input
+      className="my-2 bg-pink-200 border-2 border-gray-600"
+      onSelect={() => console.log("Selected!")}
+      placeholder="Hello Hello World"
+    />
+  );
+};
+
+export default Event6;
