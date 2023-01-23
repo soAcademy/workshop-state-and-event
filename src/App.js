711aa6cad5cdbf5aa5036bee81437f9e3db96979
Bond Rungrot <st122150@ait.asia>
2023-01-23 13:52:45 +0700

    init

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import Event5 from './Events/Event5';
 import Event6 from './Events/Event6';
 import Event7 from './Events/Event7';
 import Event8 from './Events/Event8';
+import State1 from './States/State1';
 
 const App = () => {
   return (
     <div>
-      <Event1 />
+      {/* <Event1 />
       <Event2 />
       <Event3 />
       <Event4 />
       <Event5 />
       <Event6 />
       <Event7 />
-      <Event8 />
+      <Event8 /> */}
 
-      {/* <State1 /> */}
+      <State1 />
       {/* <State2 /> */}
       {/* <State3 /> */}
       {/* <State4 /> */}
---
+++ src/States/State1.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const State1 = () => {
+  const [title, setTitle] = useState("Bin");
+
+  return (
+    <div className="bg-red-200">
+      <p>{title}</p>
+      <p>
+        <button className="p-4 bg-blue-200" onClick={() => setTitle("Jam")}>
+          Change Title
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default State1;
+
+//ปกติเอาไว้ใช้ตอนไหนเหรอครับ
