a2536b94571391293da486ce17bdf89169e3bbe5
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-23 10:11:26 +0700

    feat: add alert event

--- src/Events/Event1.js
+++ src/Events/Event1.js
@@ -3,7 +3,7 @@ import React from "react";
 const Event1 = () => {
   return (
     <div className="my-2">
-      <button className="bg-red-200" onClick={() => console.log("Hello world")}>
+      <button className="bg-red-200" onClick={() => alert("Hello world")}>
         Button 1
       </button>
     </div>
