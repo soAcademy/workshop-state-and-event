75e2383765e772e5e7b5e46f9d2bb5c1b91731cb
Your name <teak.yuktanand@gmail.com>
2023-01-23 11:16:58 +0700

    feat add Event5

---
+++ src/Events/Event5.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Event5 = () => {
+  return (
+    <div className="my-2 mx-12 bg-red-300 h-32" onMouseOver={() => console.log("Hello 5")}>
+      Hover 5
+    </div>
+  );
+};
+
+export default Event5;
