cd82bbdb08abe7829f405fcfa00af8d1e85fe916
jaiieth <midzty.rb128@gmail.com>
2023-01-23 15:21:47 +0700

    fix: refactor State6.js

--- src/States/State6.js
+++ src/States/State6.js
@@ -1,32 +1,45 @@
 import { useState } from "react";
 
 const State6 = () => {
-  const [number, setNumber] = useState(0);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [sum, setSum] = useState();
   const calculate = (e) => {
-    e.preventDefault()
-    setNumber(Number(e.target.form['number1'].value) + Number(e.target.form['number2'].value))
-  }
+    e.preventDefault();
+    setSum(Number(number1) + Number(number2));
+  };
   return (
     <div className="flex flex-col w-fit m-2 p-2 bg-gray-300 rounded-lg items-center">
       <form>
-      <input
-        type="number"
-        placeholder="Number1"
-        className="bg-gray-200 rounded-lg m-2 w-24 px-2"
-        id="number1"
-        onChange={(e)=>setNumber(e.target.value)}
-      />
-      +
-      <input
-        type="number"
-        placeholder="Number2"
-        className="bg-gray-200 rounded-lg m-2 w-24 px-2"
-        id="number2"
-      />
-      <div className="flex w-full justify-evenly">
-        <button className="px-4 mx-2 bg-gray-400 rounded-lg" onClick={calculate}>Calculate</button>
-        <p className="mx-2 bg-gray-100 rounded-lg w-fit px-4 text-red-500">{number}</p>
-      </div>
+        <input
+          type="number"
+          placeholder="Number1"
+          className="bg-gray-200 rounded-lg m-2 w-24 px-2"
+          id="number1"
+          onChange={(e) => {
+            setSum(e.target.value);
+            setNumber1(e.target.value);
+          }}
+        />
+        +
+        <input
+          type="number"
+          placeholder="Number2"
+          className="bg-gray-200 rounded-lg m-2 w-24 px-2"
+          id="number2"
+          onChange={(e) => setNumber2(e.target.value)}
+        />
+        <div className="flex w-full justify-evenly">
+          <button
+            className="px-4 mx-2 bg-gray-400 rounded-lg"
+            onClick={calculate}
+          >
+            Calculate
+          </button>
+          <p className="mx-2 bg-gray-100 rounded-lg w-fit px-4 text-red-500">
+            {sum}
+          </p>
+        </div>
       </form>
     </div>
   );
