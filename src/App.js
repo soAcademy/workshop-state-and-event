39104d5a476e9a5012d871a669c196786bb4829e
Firstweera <firstweera357@gmail.com>
2023-01-23 14:56:58 +0700

    feat: State5

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ import State1 from "./States/State1";
 import State2 from "./States/State2";
 import State3 from "./States/State3";
 import State4 from "./States/State4";
+import State5 from "./States/State5";
 
 const App = () => {
   return (
@@ -57,7 +58,7 @@ const App = () => {
       <State2 />
       <State3 />
       <State4 />
-      {/* <State5 /> */}
+      <State5 />
       {/* <State6 /> */}
 
       {/* <Prop1 data="Hello World" /> */}
---
+++ src/States/State5.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const State5 = () => {
+  const [customers, setCustomers] = useState({
+    name: "Bin",
+    location: "Bangkok",
+  });
+
+  return (
+    <div className="m-3 bg-blue-400 rounded-lg">
+      <h1 className="text-2xl p-2 mt-2">{JSON.stringify(customers)}</h1>
+      <button
+        onClick={() => setCustomers({ name: "Jam", location: "Nonthaburi" })}
+        className="w-48 h-12 border-2 rounded-lg text-lg test-center bg-slate-100 m-2"
+      >
+        Change Customer
+      </button>
+      <button
+        onClick={() => setCustomers({name: "Bin", location: "Bangkok"})}
+        className="w-48 h-12 border-2 rounded-lg text-lg test-center bg-slate-100 m-2"
+      >
+        Change Customer
+      </button>
+    </div>
+  );
+};
+
+export default State5;
