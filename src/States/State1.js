d81612db5d12ea50d757c69e96e479d889b2c63b
Your name <teak.yuktanand@gmail.com>
2023-01-23 15:42:40 +0700

    feat: State1 update

--- src/States/State1.js
+++ src/States/State1.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const State1 = () => {
   const [title, setTitle] = useState("Bin");
   return (
-    <div className="bg-blue-200">
+    <div className="bg-blue-200 p-2 m-2">
       <h1>{title}</h1>
       <button className="bg-red-200 rounded-lg p-2 m-2" onClick={() => setTitle("Jam")}>
         
