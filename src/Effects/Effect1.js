de0e07175bbf929e7ad971aaba924123bfeb2631
mplug <oppoalien@gmail.com>
2023-01-24 13:37:33 +0700

    feat: do workshop useeffect1.1

--- src/Effects/Effect1.js
+++ src/Effects/Effect1.js
@@ -1,27 +1,38 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 const Effect1 = () => {
-
   const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(100);
 
   useEffect(() => {
-    console.log(counter);
+    console.log("counter: " + counter);
 
     document.title = `Counter: ${counter}`;
-
-  },[counter]);
+  });
 
   return (
     <div className="w-100 h-screen flex justify-center items-center">
-      <div className="m-2">
-        <p className="text-center">{`Counter: ${counter}`}</p>
-        <button
-          type="button"
-          className="bg-red-200 rounded-lg shadow-md active:bg-red-300 active:shadow-lg p-2 mt-4"
-          onClick={() => setCounter(counter + 1)}
-        >
-          Click + 1
-        </button>
+      <div className="flex">
+        <div className="m-4">
+          <p className="text-center">{`Counter: ${counter}`}</p>
+          <button
+            type="button"
+            className="bg-red-200 rounded-lg shadow-md active:bg-red-300 active:shadow-lg p-2 mt-4"
+            onClick={() => setCounter(counter + 1)}
+          >
+            Click + 1
+          </button>
+        </div>
+        <div className="m-4">
+          <p className="text-center">{`Counter: ${counter2}`}</p>
+          <button
+            type="button"
+            className="bg-green-200 rounded-lg shadow-md active:bg-green-300 active:shadow-lg p-2 mt-4"
+            onClick={() => setCounter2(counter2 + 1)}
+          >
+            Click + 1
+          </button>
+        </div>
       </div>
     </div>
   );
