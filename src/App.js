c008953ce5478817ad301163b612707881327128
Poom <poomhan@gmail.com>
2023-01-24 15:57:16 +0700

    effect5

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@
 // import Effect1_1 from "./Effects/Effect1_1";
 // import Effect2 from "./Effects/Effect2";
 // import Effect3 from "./Effects/Effect3";
-import Effect4 from "./Effects/Effect4";
+// import Effect4 from "./Effects/Effect4";
+import Effect5 from "./Effects/Effect5";
 const App = () => {
   return (
     <div>
@@ -50,7 +51,8 @@ const App = () => {
       {/* <Effect1_1/> */}
       {/* <Effect2/> */}
       {/* <Effect3/> */}
-      <Effect4/>
+      {/* <Effect4/> */}
+      <Effect5/>
     </div>
   );
 };
---
+++ src/Effects/Effect5.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Effect5 = () => {
+  const [menus, setMenus] = useState();
+  //ใน usestate ห้ามมี "" เพราะมันแปลง undefined ให้เป็น "" ทำให้ run 21 error
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://api.sampleapis.com/coffee/hot",
+    }).then((response) => {
+      console.log(response.data);
+      setMenus(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      {/* <p>{JSON.stringify(menus)}</p> */}
+      {menus?.map((r) => 
+        <div className="bg-red-700 mb-2">{r.title}</div>
+      )}
+      {/* {menus !== undefined &&
+        menus.map((r) => <div className="bg-red-700 mb-2">{r.title}</div>)} */}
+    </>
+  );
+};
+
+export default Effect5;
