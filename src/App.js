3c16b362eb5709aacf98e7f53940ef39295a6bb4
Bond Rungrot <st122150@ait.asia>
2023-01-24 13:22:24 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import Event1 from './Events/Event1';
-import Event2 from './Events/Event2';
-import Event3 from './Events/Event3';
-import Event4 from './Events/Event4';
-import Event5 from './Events/Event5';
-import Event6 from './Events/Event6';
-import Event7 from './Events/Event7';
-import Event8 from './Events/Event8';
-import State1 from './States/State1';
-import State2 from './States/State2';
-import State3 from './States/State3';
-import State4 from './States/State4';
-import State5 from './States/State5';
-import State6 from './States/State6';
-import State7 from './States/State7';
-import Prop1 from './Props/Prop1';
-import Prop2 from './Props/Prop2';
-import Prop3 from './Props/Prop3';
-import Prop4 from './Props/Prop4';
-
+// import Event1 from './Events/Event1';
+// import Event2 from './Events/Event2';
+// import Event3 from './Events/Event3';
+// import Event4 from './Events/Event4';
+// import Event5 from './Events/Event5';
+// import Event6 from './Events/Event6';
+// import Event7 from './Events/Event7';
+// import Event8 from './Events/Event8';
+// import State1 from './States/State1';
+// import State2 from './States/State2';
+// import State3 from './States/State3';
+// import State4 from './States/State4';
+// import State5 from './States/State5';
+// import State6 from './States/State6';
+// import State7 from './States/State7';
+// import Prop1 from './Props/Prop1';
+// import Prop2 from './Props/Prop2';
+// import Prop3 from './Props/Prop3';
+// import Prop4 from './Props/Prop4';
+import Effect1 from './Effects/Effect1';
 
 
 const App = () => {
@@ -41,14 +41,14 @@ const App = () => {
       <State7/> */}
 
 
-      <Prop1 data="Hello Bond" />
+      {/* <Prop1 data="Hello Bond" />
       <p></p>
       <Prop2 data1="Hello World" data2="Bond Krab Phom" />
       <p></p>
       <Prop3 />
-      <Prop4 />
+      <Prop4 /> */}
 
-      {/* <Effect1 /> */}
+      <Effect1 />
       {/* <Effect2 /> */}
       {/* <Effect3 /> */}
       {/* <Effect4 /> */}
---
+++ src/Effects/Effect1.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+const Effect1 = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = `Counter ${counter}`;
+    console.log(counter);
+  }, [counter]);
+
+  return (
+    <>
+      <button
+        className="bg-yellow-300 w-24"
+        onClick={() => setCounter(counter + 1)}
+      >
+        Increase
+      </button>
+    </>
+  );
+};
+
+export default Effect1;
