3c7df5b7b205e402bb900e6f5d0b5ef020f9f44e
Poom <poomhan@gmail.com>
2023-01-24 10:02:20 +0700

    prop

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@
 // import State4 from "./States/State4";
 // import State5 from "./States/State5";
 // import State6 from "./States/State6";
-import State7 from "./States/State7";
+// import State7 from "./States/State7";
+
+import Prop1 from "./Props/Prop1";
 const App = () => {
   return (
     <div>
@@ -31,7 +33,8 @@ const App = () => {
       {/* <State4/> */}
       {/* <State5/> */}
       {/* <State6/> */}
-      <State7 />
+      {/* <State7 /> */}
+      <Prop1 data={"Hello World"}/>
     </div>
   );
 };
