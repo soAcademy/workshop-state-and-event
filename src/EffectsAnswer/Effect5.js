dc9d306c00ee2c230360c7de700338cb0a71b397
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-24 16:06:24 +0700

    await

--- src/EffectsAnswer/Effect5.js
+++ src/EffectsAnswer/Effect5.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const Effect5 = () => {
   const [menus, setMenus] = useState();
 
+  // parallel
   useEffect(() => {
     axios({
       method: "get",
@@ -15,12 +16,23 @@ const Effect5 = () => {
     // async
   }, []);
 
+  // sequential
+  // useEffect(async () => {
+  //   const response = await axios({
+  //     method: "get",
+  //     url: "https://api.sampleapis.com/coffee/hot",
+  //   });
+
+  //   console.log(response.data);
+  //   setMenus(response.data);
+  // }, []);
+
   return (
     <>
       {/* <p>{JSON.stringify(data)}</p> */}
-      {
-        menus?.map((r) => <div className="bg-red-700 mb-2">{r.title}</div>)
-      }
+      {menus?.map((r) => (
+        <div className="bg-red-700 mb-2">{r.title}</div>
+      ))}
       {/* {menus !== undefined &&
         menus.map((r) => <div className="bg-red-700 mb-2">{r.title}</div>)} */}
     </>
