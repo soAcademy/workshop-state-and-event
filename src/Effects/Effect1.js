9faeecd69465281dbd4333293d9476b769619a01
Your name <teak.yuktanand@gmail.com>
2023-01-24 13:23:27 +0700

    feat:add Effect1

---
+++ src/Effects/Effect1.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+const Effect1 = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = `Counter ${counter}`;
+  }, [counter]);
+  return (
+    <>
+      <button
+        className="bg-red-200 rounded-lg m-2 p-2"
+        onClick={() => setCounter(counter + 1)}
+      >
+        Increase
+      </button>
+    </>
+  );
+};
+
+export default Effect1;
