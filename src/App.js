09c8e1dbbc12490d2f5998f55c7b91c436ad5a9a
jaiieth <midzty.rb128@gmail.com>
2023-01-24 14:05:47 +0700

    feat: add Effect2.js

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ import Prop4 from "./Props/Prop4";
 import Effect1 from "./Effects/Effect1";
 import Effect1_1 from "./Effects/Effect1_1";
 import Effect2 from "./Effects/Effect2";
-// import Effect3 from "./Effects/Effect3";
-// import Effect4 from "./Effects/Effect4";
+import Effect3 from "./Effects/Effect3";
+import Effect4 from "./Effects/Effect4";
 // import Effect5 from "./Effects/Effect5";
 // import Effect6 from "./Effects/Effect6";
 // import Effect7 from "./Effects/Effect7";
@@ -57,8 +57,8 @@ const App = () => {
       <Effect1 />
       <Effect1_1 />
       <Effect2 />
-      {/* <Effect3 /> */}
-      {/* <Effect4 /> */}
+      <Effect3 />
+      <Effect4 />
       {/* <Effect5 /> */}
       {/* <Effect6 /> */}
       {/* <Effect7 /> */}
--- src/Effects/Effect2.js
+++ src/Effects/Effect2.js
@@ -4,16 +4,21 @@ const Effect2 = () => {
   const [name, setName] = useState("Bin");
   const [profile, setProfile] = useState("My name is Bin");
 
-  useEffect(()=> {
-    setProfile(`My name is ${name}`)
-  },[name])
+  useEffect(() => {
+    setProfile(`My name is ${name}`);
+  }, [name]);
 
   return (
-    <div className="m-2 p-2 bg-gray-300 rounded-lg w-fit">
-      <input type='text' placeholder="Enter Name" onChange={(e)=>{setName(e.target.value)}} className="rounded-lg"></input>
-      name : {name} profile : {profile}
+    <div className="bg-gray-300 rounded-lg m-2 p-2 w-fit ">
+      <input
+        type="text"
+        placeholder="Enter Name"
+        onChange={(e) => setName(e.target.value)}
+        className="rounded-lg px-2"
+      ></input>
+      <p>Name : {name}</p>
+      <p>Profile : {profile}</p>
     </div>
   );
 };
-
 export default Effect2;
