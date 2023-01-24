89f630a8370ec689478f6912c48cd66698607831
Your name <teak.yuktanand@gmail.com>
2023-01-24 13:41:12 +0700

    feat: add counter2

--- src/Effects/Effect1.js
+++ src/Effects/Effect1.js
@@ -2,16 +2,24 @@ import React, { useState, useEffect } from "react";
 
 const Effect1 = () => {
   const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
 
   useEffect(() => {
     document.title = `Counter ${counter}`;
     console.log(counter);
-  }, [counter]);
+  }, [counter, counter2]);
   return (
     <>
+      {counter}, {counter2}
       <button
         className="bg-red-200 rounded-lg m-2 p-2"
         onClick={() => setCounter(counter + 1)}
+      >
+        Increase
+      </button>
+      <button
+        className="bg-yellow-200 rounded-lg m-2 p-2"
+        onClick={() => setCounter2(counter2 + 1)}
       >
         Increase
       </button>
