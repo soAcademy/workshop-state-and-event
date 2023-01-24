606d89feeaaddef48f780fba6276c0a21b5806c6
korana <korayut@gmail.com>
2023-01-24 13:43:18 +0700

    feat: add effect1_1

--- src/App.js
+++ src/App.js
@@ -1,47 +1,26 @@
-<<<<<<< HEAD
-import Event1 from "./EventsAnswer/Event1";
-import Event2 from "./EventsAnswer/Event2";
-import Event3 from "./EventsAnswer/Event3";
-import Event4 from "./EventsAnswer/Event4";
-import Event5 from "./EventsAnswer/Event5";
-import Event6 from "./EventsAnswer/Event6";
-import Event7 from "./EventsAnswer/Event7";
-import Event8 from "./EventsAnswer/Event8";
+// import Event1 from "./EventsAnswer/Event1";
+// import Event2 from "./EventsAnswer/Event2";
+// import Event3 from "./EventsAnswer/Event3";
+// import Event4 from "./EventsAnswer/Event4";
+// import Event5 from "./EventsAnswer/Event5";
+// import Event6 from "./EventsAnswer/Event6";
+// import Event7 from "./EventsAnswer/Event7";
+// import Event8 from "./EventsAnswer/Event8";
 
-import State1 from "./StatesAnswer/State1";
-import State2 from "./StatesAnswer/State2";
-import State3 from "./StatesAnswer/State3";
-import State4 from "./StatesAnswer/State4";
-import State5 from "./StatesAnswer/State5";
-import State6 from "./StatesAnswer/State6";
+// import State1 from "./StatesAnswer/State1";
+// import State2 from "./StatesAnswer/State2";
+// import State3 from "./StatesAnswer/State3";
+// import State4 from "./StatesAnswer/State4";
+// import State5 from "./StatesAnswer/State5";
+// import State6 from "./StatesAnswer/State6";
 
-import Prop1 from "./PropsAnswer/Prop1";
-import Prop2 from "./PropsAnswer/Prop2";
-import Prop3 from "./PropsAnswer/Prop3";
-import Prop4 from "./PropsAnswer/Prop4";
+// import Prop1 from "./PropsAnswer/Prop1";
+// import Prop2 from "./PropsAnswer/Prop2";
+// import Prop3 from "./PropsAnswer/Prop3";
+// import Prop4 from "./PropsAnswer/Prop4";
 
 import Effect1 from "./Effects/Effect1";
-=======
-import Event1 from "./Events/Event1.js";
-import Event2 from "./Events/Event2.js";
-import Event3 from "./Events/Event3.js";
-import Event4 from "./Events/Event4.js";
-import Event5 from "./Events/Event5.js";
-import Event6 from "./Events/Event6.js";
-import Event7 from "./Events/Event7.js";
-import Event8 from "./Events/Event8.js";
-import State1 from "./States/State1.js";
-import State2 from "./States/State2.js";
-import State3 from "./States/State3.js";
-import State4 from "./States/State4.js";
-import State5 from "./States/State5.js";
-import State6 from "./States/State6.js";
-import State7 from "./States/State7.js";
-import Prop1 from "./Props/Prop1.js";
-import Prop2 from "./Props/Prop2.js";
-import Prop3 from "./Props/Prop3.js";
-
->>>>>>> b0f6738d0b7c5c83ebbcc1e0f798fdb92d742d0e
+import Effect1_1 from "./Effects/Effect1_1";
 
 
 const App = () => {
@@ -61,26 +40,20 @@ const App = () => {
       {/* <State4 /> */}
       {/* <State5 /> */}
       {/* <State6 /> */}
-<<<<<<< HEAD
 
       {/* <Prop1 data="Hello World" /> */}
       {/* <Prop2 data1="Hello World" data2="Bin Bin Bin" /> */}
       {/* <Prop3 /> */}
       {/* <Prop4 /> */}
 
-      <Effect1 />
+      {/* <Effect1 /> */}
+      <Effect1_1 />
       {/* <Effect2 /> */}
       {/* <Effect3 /> */}
       {/* <Effect4 /> */}
       {/* <Effect5 /> */}
       {/* <Effect6 /> */}
       {/* <Effect7 /> */}
-=======
-      {/* <State7 /> */}
-      {/* <Prop1 data="Hello World"/> */}
-      {/* <Prop2 data1="Hello World" data2="Hello Na"/> */}
-      <Prop3 />
->>>>>>> b0f6738d0b7c5c83ebbcc1e0f798fdb92d742d0e
     </div>
   );
 };
--- src/Effects/Effect1_1.js
+++ src/Effects/Effect1_1.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect} from "react";
 
-const Effect1 = () => {
+const Effect1_1 = () => {
     const [counter, setCounter] = useState(0);
+    const [counter2, setCounter2] = useState(100);
 
     useEffect(() => {
         document.title = `Counter ${counter}`;
         console.log(counter);
-    }, [counter]);
+    });
 
     return (
         <>
-            {counter}
+            {counter}, {counter2}
             <button
                 className="bg-yellow-300 w-24"
                 onClick={() => setCounter(counter + 1)}
             >
                 Increase
-            </button>         
+            </button>
+            <button
+                className="bg-yellow-300 w-24"
+                onClick={() => setCounter2(counter2 + 1)}
+            >
+                Increase2
+            </button>             
         </>
     );
 }
 
-export default Effect1;
+export default Effect1_1;
