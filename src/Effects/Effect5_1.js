864e06ef1ef579c5d3e73d23eaa6c3228a5b2eb8
Your name <teak.yuktanand@gmail.com>
2023-01-24 21:59:34 +0700

    feat: add Effect5_1

---
+++ src/Effects/Effect5_1.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const Effect5_1 = () => {
+  const [menus, setMenus] = useState();
+
+  useEffect(() => {
+    axios({ method: "get", url: `https://api.sampleapis.com/coffee/hot` }).then(
+      (response) => {
+        console.log(response.data);
+        setMenus(response.data);
+      }
+    );
+  }, []);
+
+  return (
+    <>
+      {/* <p>{JSON.stringify(data)}</p> */}
+      {menus?.map((r) => (
+        <div className="bg-blue-500 mb-2">{r.title}</div>
+      ))}
+    </>
+  );
+};
+
+export default Effect5_1;
