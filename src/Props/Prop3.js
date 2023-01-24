e5671adcda317fd9db0bef0e54748355f65af58d
mplug <oppoalien@gmail.com>
2023-01-24 10:45:08 +0700

    feat: do workshop prop3

---
+++ src/Props/Prop3.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const RenderComponent = (props) => <div>{props.dataCnt}</div>;
+
+const Prop3 = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="w-100 h-screen flex justify-center items-center">
+      <div className="">
+        <RenderComponent dataCnt={counter} />
+        <button
+          onClick={() => setCounter(counter + 1)}
+          className="bg-green-100 active:bg-green-300 border-2 border-green-300 rounded-lg font-bold shadow-md active:shadow-lg py-2 px-3"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Prop3;
