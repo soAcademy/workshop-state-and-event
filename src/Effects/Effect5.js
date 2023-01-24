2e857407733fd74ebf0a98c3df6aae92e19dadb4
Firstweera <firstweera357@gmail.com>
2023-01-24 15:44:28 +0700

    feat: Effect5 - Manus

--- src/Effects/Effect5.js
+++ src/Effects/Effect5.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Effect5 = () => {
-  const [data, setData] = useState("");
+  // const [data, setData] = useState("");
+  const [manus, setManus] = useState();
 
   useEffect(() => {
     axios({
@@ -10,13 +11,15 @@ const Effect5 = () => {
       url: "https://api.sampleapis.com/coffee/hot",
     }).then((response) => {
       console.log(response);
-      setData(response);
+      // setData(response);
+      setManus(response.data);
     });
   }, []);
 
   return (
     <div className="m-3 py-2 bg-yellow-200 rounded-lg">
-      <p className="my-2 px-2">{JSON.stringify(data)}</p>
+      {/* <p className="my-2 px-2">{JSON.stringify(data)}</p> */}
+      {manus?.map((r) => <div className="my-2 px-2">{r.title}</div>)}
     </div>
   );
 };
