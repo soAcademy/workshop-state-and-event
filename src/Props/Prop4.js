0937af2c797d4afdb190fc3f4fd00d47f1f67f29
Your name <teak.yuktanand@gmail.com>
2023-01-24 11:26:15 +0700

    feat: add Prop4

---
+++ src/Props/Prop4.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+const ComponentIncrease = ({ counter, setCounter }) => (
+  <div>
+    <button
+      className="bg-green-200 m-3 rounded-lg p-2"
+      onClick={() => setCounter(counter + 1)}
+    >
+      Increase
+    </button>
+  </div>
+);
+const ComponentDecrease = ({ counter, setCounter }) => (
+  <div>
+    <button
+      className="bg-red-200 m-3 rounded-lg p-2"
+      onClick={() => setCounter(counter - 1)}
+    >
+      Decrease
+    </button>
+  </div>
+);
+const Prop4 = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <>
+      {counter}
+      <ComponentIncrease counter={counter} setCounter={setCounter} />
+      <ComponentDecrease counter={counter} setCounter={setCounter} />
+    </>
+  );
+};
+
+export default Prop4;
