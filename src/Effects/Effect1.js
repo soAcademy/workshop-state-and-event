e3f0bba6c6ebf53304b8705a527fa63b8718fe53
kunlanis <yoo.kunlanis@gmail.com>
2023-01-24 13:47:26 +0700

    feat:useEffect1-1

--- src/Effects/Effect1.js
+++ src/Effects/Effect1.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 // component 1
 const RenderComponent = (props) => <div>Count = {props.count}</div>;
 // component 2
@@ -29,24 +29,29 @@ const MinusComponent = ({ count, setCount }) => (
 );
 
 const Effect1 = () => {
-   const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(100);
 
-   useEffect(() => {
+  useEffect(() => {
     //write code to call when state changes
-     document.title = `Counter ${count}`;
+    document.title = `Counter ${count}`;
+    console.log(count);
     //  return () => {
-    //    write code to clean up 
+    //    write code to clean up
     //  }
-   }, [count])
-   
+  }, );
+
   return (
-<div className="bg-red-200">
-    {/* component 1 */}
-    <RenderComponent count={count} />
-    {/* component 2 */}
-    <PlusComponent count={count} setCount={setCount}/>
-    {/* component 3 */}
-    <MinusComponent count={count} setCount={setCount}/>
+    <div className="bg-red-200">
+      {/* component 1 */}
+      <RenderComponent count={count} />
+      {/* component 2 */}
+      <PlusComponent count={count} setCount={setCount} />
+      
+      {/* component 1 */}
+      <RenderComponent count={count2} />
+      {/* component 3 */}
+      <MinusComponent count={count2} setCount={setCount2} />
     </div>
   );
 };
