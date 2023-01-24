3daafbaba8e55f2727d873e8613dab130d89d136
Your name <teak.yuktanand@gmail.com>
2023-01-24 10:45:35 +0700

    feat: add Prop3

---
+++ src/Props/Prop3.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+const RenderComponent = ({ counterData }) => <div>counter: {counterData}</div>;
+const Prop3 = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <>
+      <RenderComponent counterData={counter} />
+      <button className="bg-red-200 m-3 rounded-lg p-2" onClick={() => setCounter(counter + 1)}>
+        Increased ticker
+      </button>
+    </>
+  );
+};
+
+export default Prop3;
