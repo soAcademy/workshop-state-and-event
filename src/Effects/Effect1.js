813ae967a73ed3d36af2647e0e129a811aa103d0
kunlanis <yoo.kunlanis@gmail.com>
2023-01-24 13:19:02 +0700

    fix:useEffect1 change button to component

--- src/Effects/Effect1.js
+++ src/Effects/Effect1.js
@@ -1,10 +1,33 @@
 import React, { useState,useEffect } from "react";
-// component 1 that render value from component 2 
-// const RenderComponent = ({ counterData }) => <div>Count = {counterData}</div>;
-const RenderComponent = ( props ) => <div>Count = {props.counterData}</div>;
+// component 1
+const RenderComponent = (props) => <div>Count = {props.count}</div>;
+// component 2
+const PlusComponent = ({ count, setCount }) => (
+  <div>
+    <button
+      className="p-4 m-1 bg-blue-200"
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      Count --
+    </button>
+  </div>
+);
+// component 3
+const MinusComponent = ({ count, setCount }) => (
+  <div>
+    <button
+      className="p-4 m-1 bg-blue-200"
+      onClick={() => {
+        setCount(count - 1);
+      }}
+    >
+      Count --
+    </button>
+  </div>
+);
 
-
-//component 2 that call component 1 then send to App.js
 const Effect1 = () => {
    const [count, setCount] = useState(0);
 
@@ -17,19 +40,13 @@ const Effect1 = () => {
    }, [count])
    
   return (
-    <div className="bg-red-200">
-      <RenderComponent counterData={count} />
-      {/* เรียกรูปแบบการแสดงผลจาก component1 แล้วส่งค่า props ไปให้ โดยการส่งค่า count ที่เกิดจากฟังก์ชั้นของ useState*/}
-        <button className="p-4 m-1 bg-blue-200" onClick={() => {
-          setCount(count + 1)
-        }}>
-          Count ++
-        </button>
-        <button className="p-4 m-1 bg-blue-200" onClick={() => {
-          setCount(count - 1)
-        }}>
-          Count --
-        </button>
+<div className="bg-red-200">
+    {/* component 1 */}
+    <RenderComponent count={count} />
+    {/* component 2 */}
+    <PlusComponent count={count} setCount={setCount}/>
+    {/* component 3 */}
+    <MinusComponent count={count} setCount={setCount}/>
     </div>
   );
 };
