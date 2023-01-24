db231bb5a614dec6975e54736e21aff812700e21
Phisit Siprasatthong <phisite@gmail.com>
2023-01-24 09:52:21 +0700

    feat: add solution for Prop1

--- src/App.js
+++ src/App.js
@@ -42,7 +42,12 @@
 // import State4 from "./States/State4";
 // import State5 from "./States/State5";
 // import State6 from "./States/State6";
-import State7 from "./States/State7";
+// import State7 from "./States/State7";
+
+import Prop1 from "./Props/Prop1";
+// import Prop2 from "./Props/Prop2";
+// import Prop3 from "./Props/Prop3";
+// import Prop4 from "./Props/Prop4";
 
 const App = () => {
   return (
@@ -62,9 +67,9 @@ const App = () => {
       {/* <State4 /> */}
       {/* <State5 /> */}
       {/* <State6 /> */}
-      <State7 />
+      {/* <State7 /> */}
 
-      {/* <Prop1 data="Hello World" /> */}
+      <Prop1 data="Hello World" />
       {/* <Prop2 data1="Hello World" data2="Bin Bin Bin" /> */}
       {/* <Prop3 /> */}
       {/* <Prop4 /> */}
---
+++ src/Props/Prop1.js
@@ -0,0 +1,3 @@
+const Prop1 = ({ data }) => <div>{data}</div>;
+
+export default Prop1;
