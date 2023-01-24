9e9605c8bb352cbfb47b3b3e93c766800ebe2a12
Bond Rungrot <st122150@ait.asia>
2023-01-24 14:50:43 +0700

    init

---
+++ src/Effects/Effect4.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+const Effect4 = () => {
+  const [countdown, setCountdown] = useState(10);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(countdown - 1);
+    }, 1000);
+
+    // if (countdown === 1) {
+    //   clearInterval(interval);
+    // }
+
+
+    //true
+    //false || true
+    // countdown > 1 && clearInterval(interval);
+    countdown > 0 || clearInterval(interval);
+
+    //true &&true
+    //toggle && <div></div>
+    return () => clearInterval(interval);
+  }, [countdown]);
+
+  return (
+    <>
+      <p>Countdown: {countdown}</p>
+    </>
+  );
+};
+
+export default Effect4;
+
+
+//ให้ใช้ usueffect ที่เป็นตัวโครงสร้างพื้รนฐานร่วมกับ HTML
+// setinterval เป็นโครงสร้างพื้นฐานจของ Javscript
+// || แปลว่า หรือหรือ (จริงอย่างใดอย่างหนุึ่งก็พอ) กับอันอีกคือ && แอน แอน (ต้องจริงทั้งคู่)
