0aa4f518d52e14ad7cd940b3064939c9adf444c4
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-25 11:19:52 +0700

    feat: router 2 workshop

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Home from "./pages/Home";
 
 const App = () => (
   <BrowserRouter>
+    <div className="bg-sky-500 w-full">This is Navbar</div>
     <Routes>
-      <Route exact path="/" element={<Home />} />
+      <Route exact path="/" element={<Home name="Hello Boeing" />} />
       <Route exact path="/about" element={<About />} />
     </Routes>
+    <div className="bg-green-500 w-full">This is footer</div>
   </BrowserRouter>
 );
 export default App;
