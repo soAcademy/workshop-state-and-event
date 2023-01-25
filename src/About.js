2b60ad8417db1c0738d61aed5ff49da15831b0d7
Your name <teak.yuktanand@gmail.com>
2023-01-25 10:51:42 +0700

    add About

---
+++ src/About.js
@@ -0,0 +1,25 @@
+import {Link} from "react-router-dom";
+
+const About = () => {
+  <div>
+    <Link to="/">Home</Link> | <span className="underline">About</span>
+  </div>;
+}
+export default About;
+
+
+
+
+
+
+
+
+// const App = () => (
+//   <BrowserRouter>
+//     <Routes>
+//       <Route exact path="/" element={<Home/>} />
+//       <Route exact path="/about" element={<About/>} />
+//     </Routes>
+//   </BrowserRouter>
+// );
+// export default App;
