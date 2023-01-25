cb42e36f52f7b5b605913318d37df459a5b982aa
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-01-25 11:01:56 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+const App=()=>(
+  <BrowserRouter>
+  <Routes>
+    <Route exact path='/' element={<Home />}/>
+    
+    <Route exact path='/about' element={<About/>}/>
 
-
-
-
-
+</Routes>
+  </BrowserRouter>
+)
+export default App;
---
+++ src/Pages/About.js
@@ -0,0 +1,8 @@
+import {Link} from "react-router-dom";
+const About =()=>(
+  <div>
+    <Link to="/">Home</Link>{""}
+    <span className="underlined">About</span>
+  </div>
+);
+export default About;
