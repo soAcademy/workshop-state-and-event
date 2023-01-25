63208b887c716bbdbba38d98cd7f1840de8a6f72
Bond Rungrot <st122150@ait.asia>
2023-01-25 11:57:04 +0700

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import { Routes } from 'react-router-dom';
 import About from "./Pages/About";
 import Home from "./Pages/Home";
+import Product1 from "./Pages/Product1";
+import Product2 from "./Pages/Product2";
 
 const App = () => {
   return (
@@ -12,7 +14,13 @@ const App = () => {
           <Routes>
             <Route exact path="/" element={<Home title="Hello Bond" />} />
             <Route exact path="/about" element={<About />} />
+            <Route exact path="products">
+              <Route exact path="product1" element={<Product1 />} />
+              <Route exact path="product2" element={<Product2 />} />
+            </Route>
+            <Route> exact path="*" element={<>404 Not Found</>} </Route>
           </Routes>
+
           <div className="bg-green-700 w-full">This is Footer</div>
         </BrowserRouter>
       </div>
