2e7612b6e22d426ce93c4f8be01885f47ea3617c
Bond Rungrot <st122150@ait.asia>
2023-01-25 11:41:55 +0700

    init

--- src/PagesAnswer/Home.js
+++ src/PagesAnswer/Home.js
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
 
-// const Home = () => (
-//   <div>
-//     <span className="underline">Home</span> |{" "}
-//     <Link to="/about">About</Link>
-//   </div>
-// );
-// export default Home;
+const Home = (props) => (
+  <div>
+    <div>{props.title}</div>
+    <div>{props.name2}</div>
+    <span className="underline">Home</span>
+    <span className="px-4">|</span>
+    <Link to="/about">
+      About
+    </Link><span className="px-4">|</span>
+    <Link to="/products/product1">
+      Product1
+    </Link><span className="px-4">|</span>
+    <Link to="/products/product2">
+    Product2
+    </Link>
+  </div>
+);
 
+export default Home;
