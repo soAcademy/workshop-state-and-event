66c9291a26dbc124c1f72e129b9720c3d969865a
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-25 11:09:33 +0700

    feat: add footer header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import About from "./PagesAnswer/About";
 
 const App = () => (
   <BrowserRouter>
+    <div className="bg-red-700 w-full">This is Navbar</div>
     <Routes>
-      <Route exact path="/" element={<Home/>} />
-      <Route exact path="/about" element={<About/>} />
+      <Route exact path="/" element={<Home title="Hello Bin" />} />
+      <Route exact path="/about" element={<About />} />
     </Routes>
+    <div className="bg-green-700 w-full">This is Footer</div>
   </BrowserRouter>
 );
 export default App;
