e854e73fb28368785cb3874b76a2823e42f2ba85
Your name01~ <poovareskkc@gmail.com>
2023-01-25 12:01:10 +0700

    editpagep1p2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Product1 from "./Pages/Product1";
+import Product2 from "./Pages/Product2";
 
 const App = () => (
   <BrowserRouter>
     <div className="bg-green-400 w-full"> This is Navbar </div>
     <Routes>
-      <Route exact path="/" element={<Home title="Hello Ball" name2="Poovares" />}
+      <Route
+        exact
+        path=""
+        element={<Home title="Hello Ball" name2="Poovares" />}
       />
-      <Route exact path="/about" element={<About/>} />
+      <Route exact path="about" element={<About />} />
+      <Route exact path="products" element={<></>} >
+        <Route exact path="product1" element={<Product1 />} />
+        <Route exact path="product2" element={<Product2 />} />
+      </Route>
+      <Route exact path="*" element={<>Products </>} />
     </Routes>
-    <div className="bg-yellow-400 w-full" >This is Footer</div>
+    <div className="bg-yellow-400 w-full">This is Footer</div>
   </BrowserRouter>
 );
-export default App
+export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,20 @@ import { Link } from "react-router-dom";
 const Home = (props) => (
   <div>
     <div>{props.title}</div>
+    <div>{props.name2}</div>
     <span className="underline ml-5">
       Home
       </span> |{" "}
+    <span className="px-4" >|</span>
       <Link to="/about">
       About
-      </Link>
+      </Link><span className="px-4" >|</span>
+      <Link to="/product/product1">
+        Product1
+      </Link><span className="px-4" >|</span>
+      <Link to="/product/product2">
+        Product2
+        </Link>
   </div>
 );
 
