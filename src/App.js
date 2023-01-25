7d5b271d167d28ae482651679429f8c88bf6791e
kunlanis <yoo.kunlanis@gmail.com>
2023-01-25 14:22:12 +0700

    feat:add nav link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
@@ -9,13 +9,19 @@ import Product2 from "./Pages/Products/product2";
 
 const App = () => (
   <>
-    <nav className="bg-blue-300">this is nav bar</nav>
     <BrowserRouter>
+      <div className="bg-blue-300">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/product">Product</Link>
+        <Link to="/products">Products</Link>
+      </div>
       <Routes>
         <Route exact path="/" element={<Home title="Cake's page" />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/contact" element={<Contact />} />
-        
+
         <Route exact path="/product" element={<Product />} />
         <Route path="product">
           <Route path=":productId" element={<Product />} />
