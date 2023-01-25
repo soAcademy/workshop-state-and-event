f5be9fde5fa73ce4896d9bdf5702e67b780c986f
Firstweera <firstweera357@gmail.com>
2023-01-25 10:39:44 +0700

    feat: Peges Home/About

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./PagesAnswer/Home";
-import About from "./PagesAnswer/About";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./PagesAnswer/Home";
+// import About from "./PagesAnswer/About";
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<Home/>} />
-      <Route exact path="/about" element={<About/>} />
-    </Routes>
-  </BrowserRouter>
-);
+// const App = () => (
+//   <BrowserRouter>
+//     <Routes>
+//       <Route exact path="/" element={<Home/>} />
+//       <Route exact path="/about" element={<About/>} />
+//     </Routes>
+//   </BrowserRouter>
+// );
+// export default App;
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 export default App;
---
+++ src/Pages/About.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <Link to="/">Home</Link> | <span class="underline">About</span>
+    </div>
+  );
+};
+
+export default About;
