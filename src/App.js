03bacda954f9295efbbcc120b521639950374dde
BornDhamachart <dhamachart.c@gmail.com>
2023-01-25 11:15:53 +0700

    feat:submit workshop

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import About from "./Pages/About";
 
 const App = () => (
   <BrowserRouter>
+    <div className="bg-red-200 w-full h-24 text-center border pt-8">
+      Nav Bar
+    </div>
     <Routes>
-      <Route exact path="/" element={<Home />} />
+      <Route exact path="/" element={<Home title="Hello Born" />} />
       <Route exact path="/about" element={<About />} />
     </Routes>
+    <div className="bg-blue-300 w-full h-24 text-center border pt-8">
+      Footer
+    </div>
   </BrowserRouter>
 );
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 
-const Home = () => (
-  <div>
-    <span className="underline">Home</span> | <Link to="/about">About</Link>
-  </div>
+const Home = (props) => (
+  <>
+    <div>
+      <span className="underline">Home</span> | <Link to="/about">About</Link>
+    </div>
+    <div>{props.title}</div>
+  </>
 );
 
 export default Home;
