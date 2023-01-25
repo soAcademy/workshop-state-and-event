d44db73432e2a766cd575ad7dcea70650fb3c161
Bond Rungrot <st122150@ait.asia>
2023-01-25 11:28:18 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Routes } from 'react-router-dom';
-import About from './Pages/About';
-import Home from './Pages/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Routes } from 'react-router-dom';
+import About from "./Pages/About";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
     <>
-  <div>
-  <BrowserRouter>
-  <div className="bg-teal-700 w-full">This is BavBar</div>
-    <Routes>
-      <Route exact path="/" element={<Home title="Hello Bond"/>} />
-      <Route exact path="/about" element={<About/>} />
-    </Routes>
-  <div className="bg-green-700 w-full">This is Footer</div>
-  </BrowserRouter>
-  </div>
-  </>
-);
+      <div>
+        <BrowserRouter>
+          <div className="bg-teal-700 w-full">This is NavBar</div>
+          <Routes>
+            <Route exact path="/" element={<Home title="Hello Bond" />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+          <div className="bg-green-700 w-full">This is Footer</div>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 };
 export default App;
