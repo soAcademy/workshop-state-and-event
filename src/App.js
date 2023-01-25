4913f41ad49e3c9f0ac5ba0950390306440576c3
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-25 11:34:33 +0700

    fix: nested route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./PagesAnswer/Home";
 import About from "./PagesAnswer/About";
+import Product1 from "./PagesAnswer/Product1";
+import Product2 from "./PagesAnswer/Product2";
 
 const App = () => (
   <BrowserRouter>
     <div className="bg-red-700 w-full">This is Navbar</div>
     <Routes>
-      <Route exact path="/" element={<Home title="Hello Bin" />} />
+      <Route
+        exact
+        path="/"
+        element={<Home title="Hello Bin" name2="Hello Name2" />}
+      />
       <Route exact path="/about" element={<About />} />
+      <Route exact path="/products/product1" element={<Product1 />} />
+      <Route exact path="/products/product2" element={<Product2 />} />
     </Routes>
     <div className="bg-green-700 w-full">This is Footer</div>
   </BrowserRouter>
--- src/PagesAnswer/Home.js
+++ src/PagesAnswer/Home.js
@@ -3,9 +3,17 @@ import { Link } from "react-router-dom";
 const Home = (props) => (
   <div>
     <div>{props.title}</div>
-    <span className="underline">Home</span> |{" "}
+    <div>{props.name2}</div>
+    <span className="underline">Home</span>
+    <span className="px-4">|</span>
     <Link to="/about">
       About
+    </Link><span className="px-4">|</span>
+    <Link to="/products/product1">
+      Product1
+    </Link><span className="px-4">|</span>
+    <Link to="/products/product2">
+    Product2
     </Link>
   </div>
 );
