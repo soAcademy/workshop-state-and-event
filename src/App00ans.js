c4453113b88b70f175eb6558f01cbf5b9ab05280
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-25 10:51:44 +0700

    feat:router workshop

--- src/App00ans.js
+++ src/App00ans.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
+import Home from "./PagesAnswer/Home";
+import About from "./PagesAnswer/About";
 
 const App = () => (
   <BrowserRouter>
