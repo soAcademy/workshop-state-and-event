c0ff73f584d487e27b336351cc41e2e8358808e0
kunlanis <yoo.kunlanis@gmail.com>
2023-01-30 11:17:27 +0700

    feat:WS-LocalStorage-4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import LocalStorage1 from "./LocalStorage/LocalStorage1";
 import LocalStorage2 from "./LocalStorage/LocalStorage2";
 import LocalStorage3 from "./LocalStorage/LocalStorage3";
-import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
+import LocalStorage4 from "./LocalStorage/LocalStorage4";
 import ToDoList1 from "./ToDoListAnswers/ToDoList1";
 import ToDoList2 from "./ToDoListAnswers/ToDoList2";
 import ToDoList3 from "./ToDoListAnswers/ToDoList3";
@@ -10,10 +10,10 @@ import ToDoList4 from "./ToDoListAnswers/ToDoList4";
 const App = () => {
   return (
     <>
-      <LocalStorage1 />
-      <LocalStorage2 />
-      <LocalStorage3 />
-      {/* <LocalStorage4 /> */}
+      {/* <LocalStorage1 /> */}
+      {/* <LocalStorage2 /> */}
+      {/* <LocalStorage3 /> */}
+      <LocalStorage4 />
       {/* <ToDoList1 /> */}
       {/* <ToDoList2 /> */}
       {/* <ToDoList3 /> */}
---
+++ src/LocalStorage/LocalStorage4.js
@@ -0,0 +1,32 @@
+const LocalStorage4 = () => {
+  const arrayData = [
+    {
+      name: "Bin",
+      location: "Bangkok",
+      age: 30,
+    },
+    {
+      name: "Jam",
+      location: "Nonthaburi",
+      age: 25,
+    },
+    {
+      name: "Ploy",
+      location: "Sukhothai",
+      age: 20,
+    },
+  ];
+  console.log(JSON.stringify(arrayData));
+  localStorage.setItem("arrayData", JSON.stringify(arrayData));
+  console.log(localStorage.getItem("arrayData"));
+
+  const data = JSON.parse(localStorage.getItem("arrayData"));
+  console.log(data);
+
+  return (
+    <>
+    {data[1].name},{data[1].location},{data[1].age}
+    </>
+  )
+}
+export default LocalStorage4;
