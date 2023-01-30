7b2a9a873910db871f8994e1522322057042a6f5
Bond Rungrot <st122150@ait.asia>
2023-01-30 10:51:26 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-// import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
-import LocalStorage2 from "./LocalStorages/LocalStorage2";
-// import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
+// import LocalStorage1 from "./LocalStorages/LocalStorage1";
+// import LocalStorage2 from "./LocalStorages/LocalStorage2";
+import LocalStorage3 from "./LocalStorages/LocalStorage3";
 // import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
-// import ToDoList1 from "./ToDoListAnswers/ToDoList1";
-// import ToDoList2 from "./ToDoListAnswers/ToDoList2";
-// import ToDoList3 from "./ToDoListAnswers/ToDoList3";
-// import ToDoList4 from "./ToDoListAnswers/ToDoList4";
+// import ToDoList1 from "./LocalStorages/ToDoList1";
+// import ToDoList2 from "./LocalStorages/ToDoList2";
+// import ToDoList3 from "./LocalStorages/ToDoList3";
+// import ToDoList4 from "./LocalStorages/ToDoList4";
 
 const App = () => {
   return (
     <>
       {/* <LocalStorage1 /> */}
-      <LocalStorage2 />
-      {/* <LocalStorage3 /> */}
+      {/* <LocalStorage2 /> */}
+      <LocalStorage3 />
       {/* <LocalStorage4 /> */}
       {/* <ToDoList1 /> */}
       {/* <ToDoList2 /> */}
---
+++ src/LocalStorages/LocalStorage3.js
@@ -0,0 +1,33 @@
+const LocalStorage3 = () => {
+
+
+
+  const objectData = {
+    name: "Bin",
+    location: "Bangkok",
+    age: 30,
+  };
+
+
+
+
+  console.log(JSON.stringify(objectData));
+  localStorage.setItem("objectData", JSON.stringify(objectData));
+  console.log(localStorage.getItem("objectData"));
+
+
+
+
+  const data = JSON.parse(localStorage.getItem("objectData"));
+  console.log(data);
+  // เราจะต้องใช้ JSON.parse เพื่อเปลงให้มันเเป็น strong ก่อน
+
+
+
+  return (
+    <>
+      {data.name}, {data.location}, {data.age}
+    </>
+  );
+};
+export default LocalStorage3;
