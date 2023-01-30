750f4274b325c228190fe154cd0ea80f0a93a790
korana <korayut@gmail.com>
2023-01-30 11:07:39 +0700

    add LocalStorage4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import LocalStorage1 from "./LocalStorage/LocalStorage1";
 import LocalStorage2 from "./LocalStorage/LocalStorage2";
 import LocalStorage3 from "./LocalStorage/LocalStorage3";
+import LocalStorage4 from "./LocalStorage/LocalStorage4";
 
 const App = () => (
   <div className="">
     {/* <LocalStorage1 />
     <LocalStorage2 /> */}
-    <LocalStorage3 />
+    {/* <LocalStorage3 /> */}
+    <LocalStorage4 />
   </div>
 );
 
