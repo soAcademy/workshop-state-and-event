80c29dcccb7161a99707b7a5f688378349739c57
jaiieth <midzty.rb128@gmail.com>
2023-01-30 10:49:56 +0700

    fix: JSON.parse

--- src/LocalStorage/LocalStorage3.js
+++ src/LocalStorage/LocalStorage3.js
@@ -3,9 +3,15 @@ const LocalStorage3 = () => {
     name: "Bin",
     location: "Bangkok",
     age: 30,
-  }
-  localStorage.setItem("objectData",JSON.stringify(objectData))
-  const object = localStorage.getItem("objectData")
-  return <div>{object}</div>
-}
-export default LocalStorage3
+  };
+  localStorage.setItem("objectData", JSON.stringify(objectData));
+  const object = JSON.parse(localStorage.getItem("objectData"));
+  return (
+    <div className="bg-red-300 w-fit rounded-lg p-2">
+      <div>Name: {object.name}</div>
+      <div>Location: {object.location}</div>
+      <div>Age: {object.age}</div>
+    </div>
+  );
+};
+export default LocalStorage3;
