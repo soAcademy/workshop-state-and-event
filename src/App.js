ef546bc6e3896e527400137d018bf9a0014f2603
Bond Rungrot <st122150@ait.asia>
2023-01-30 10:13:35 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
+// import LocalStorage2 from "./LocalStorageAnswer/LocalStorage2";
+// import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
+// import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
+// import ToDoList1 from "./ToDoListAnswers/ToDoList1";
+// import ToDoList2 from "./ToDoListAnswers/ToDoList2";
+// import ToDoList3 from "./ToDoListAnswers/ToDoList3";
+// import ToDoList4 from "./ToDoListAnswers/ToDoList4";
+
+const App = () => {
+  return (
+    <>
+      <LocalStorage1 />
+      {/* <LocalStorage2 /> */}
+      {/* <LocalStorage3 /> */}
+      {/* <LocalStorage4 /> */}
+      {/* <ToDoList1 /> */}
+      {/* <ToDoList2 /> */}
+      {/* <ToDoList3 /> */}
+      {/* <ToDoList4 /> */}
+    </>
+  );
+};
+
+export default App;
