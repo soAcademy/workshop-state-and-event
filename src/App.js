d5c3555d0c31a8bfd019f00d55499ee833ffe108
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-30 10:24:35 +0700

    feat: localstorage2 workshop done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Product1 from "./pages/Product1";
-import Product2 from "./pages/Product2";
-import Product from "./pages/Product";
-import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
-import LocalStorage2 from "./LocalStorageAnswer/LocalStorage2";
-import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
-import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
-import ToDoList1 from "./ToDoListAnswers/ToDoList1";
-import ToDoList2 from "./ToDoListAnswers/ToDoList2";
-import ToDoList3 from "./ToDoListAnswers/ToDoList3";
-import ToDoList4 from "./ToDoListAnswers/ToDoList4";
+import React from "react";
+import LocalStorage1 from "./LocalStorage/LocalStorage1";
+import LocalStorage2 from "./LocalStorage/LocalStorage2";
 
-const App = () => (
-  <BrowserRouter>
-    <div className="bg-sky-500 w-full">This is Navbar</div>
-    <Routes>
-      <Route exact path="/" element={<Home name="Hello Boeing" />} />
-      <Route exact path="/about" element={<About />} />
+const App = () => {
+  return (
+    <>
+      <LocalStorage2 />
+      <LocalStorage1 />
+    </>
+  );
+};
 
-      <Route path="Product">
-        <Route path=":productId" element={<Product />} />
-      </Route>
-      {/* : ทำให้ useParam หา productId */}
-      {/* //localhost:3000/Product/>>...<<  */}
-
-      <Route exact path="products">
-        <Route exact path="product1" element={<Product1 />} />
-        <Route exact path="product2" element={<Product2 />} />
-      </Route>
-      <Route exact path="*" element={<>404 Not found</>} />
-    </Routes>
-    <div className="bg-green-500 w-full">This is footer</div>
-  </BrowserRouter>
-);
 export default App;
---
+++ src/LocalStorage/LocalStorage2.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LocalStorage2 = () => {
+  localStorage.setItem("num", 14);
+  const num = localStorage.getItem("num");
+  return <>{Number(num + 3)}</>;
+};
+
+export default LocalStorage2;
