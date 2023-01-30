af41066e3caec4de2928544c6e55e2cd914effa4
korana <korayut@gmail.com>
2023-01-30 10:24:03 +0700

    Add Localstorage2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import LocalStorage1 from "./LocalStorage/LocalStorage1";
+import LocalStorage2 from "./LocalStorage/LocalStorage2";
 
 const App = () => (
   <div className="">
     <LocalStorage1 />
+    <LocalStorage2 />
   </div>
 );
 
---
+++ src/LocalStorage/LocalStorage2.js
@@ -0,0 +1,9 @@
+const LocalStorage2 = () => {
+  //localStorage.setItem("num", 14)
+
+  const num = localStorage.getItem("num")
+
+  return <>&nbsp;&nbsp;{Number(num)+3}</>
+}
+
+export default LocalStorage2
