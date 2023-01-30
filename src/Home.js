07189ec7d2fc64552e7b880907beba4e76fafff5
Your name <teak.yuktanand@gmail.com>
2023-01-30 10:01:26 +0700

    feat:update

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,8 @@ const Home = (props) => (
     <Link to="/products/product1">product1</Link>
     <span className="px-2">|</span>
     <Link to="/products/product2">product2</Link>
+    <span className="px-2">|</span>
+    <Link to="/product/productId">product</Link>
   </div>
 );
 export default Home;
