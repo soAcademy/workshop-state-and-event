5ece004acbff4bab2f9a17b9a6657bc507da02c5
Poom <poomhan@gmail.com>
2023-01-30 11:16:43 +0700

    4

--- src/LocalStorage/LocalStorage3.js
+++ src/LocalStorage/LocalStorage3.js
@@ -7,13 +7,13 @@ const LocalStorage3 = () => {
   // When sending data to a web server, the data has to be a string.
   // Convert a JavaScript object into a string with JSON.stringify().
 
-  console.log(JSON.stringify(objectData));
+  console.log("JSON.stringify", JSON.stringify(objectData));
   localStorage.setItem("objectData", JSON.stringify(objectData));
 
   console.log(localStorage.getItem("objectData"));
   // Parse the data with JSON.parse(), and the data becomes a JavaScript object.
   const data = JSON.parse(localStorage.getItem("objectData"));
-  console.log(data);
+  console.log("JSON.parse",data);
 
   return (
     <>
