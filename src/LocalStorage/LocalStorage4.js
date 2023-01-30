2baf15090b2cbb43a27ad1ea6d5b71ff17709e06
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-01-30 23:55:19 +0700

    Update

--- src/LocalStorage/LocalStorage4.js
+++ src/LocalStorage/LocalStorage4.js
@@ -16,10 +16,11 @@ const LocalStorage4 = ()=>{
       tel : 1789
     }
   ];
-  console.log("ArrayOfObject",ArrayOfObject);
+  console.log(">>> ArrayOfObject",ArrayOfObject);
   localStorage.setItem("data",JSON.stringify(ArrayOfObject));   //Save   OBJ to String 
 
   console.log("JSON.stringify",localStorage.getItem("data"))
+  console.log("JSON.parse",JSON.parse(localStorage.getItem("data")));
   const arr = JSON.parse(localStorage.getItem("data"));  //Get + Transform String to OBJ 
   return(
     <>{arr[0].name}, {arr[0].location},{arr[0].tel}</>
