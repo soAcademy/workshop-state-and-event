f573b931d56e0bb4898f12b0f85a813b0f101c8b
kunlanis <yoo.kunlanis@gmail.com>
2023-01-31 10:21:57 +0700

    feat:WS-ToDoList-2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import LocalStorage2 from "./LocalStorage/LocalStorage2";
 import LocalStorage3 from "./LocalStorage/LocalStorage3";
 import LocalStorage4 from "./LocalStorage/LocalStorage4";
 import ToDoList1 from "./ToDoList/ToDoList1";
-import ToDoList2 from "./ToDoListAnswers/ToDoList2";
+import ToDoList2 from "./ToDoList/ToDoList2";
 import ToDoList3 from "./ToDoListAnswers/ToDoList3";
 import ToDoList4 from "./ToDoListAnswers/ToDoList4";
 
@@ -14,8 +14,8 @@ const App = () => {
       {/* <LocalStorage2 /> */}
       {/* <LocalStorage3 /> */}
       {/* <LocalStorage4 /> */}
-      <ToDoList1 />
-      {/* <ToDoList2 /> */}
+      {/* <ToDoList1 /> */}
+      <ToDoList2 />
       {/* <ToDoList3 /> */}
       {/* <ToDoList4 /> */}
     </>
