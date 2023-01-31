4617ab5bcb966845c02e7f4b6c6280941a4d7015
korana <korayut@gmail.com>
2023-01-31 09:45:54 +0700

    add Todolist1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import LocalStorage2 from "./LocalStorage/LocalStorage2";
 import LocalStorage3 from "./LocalStorage/LocalStorage3";
 import LocalStorage4 from "./LocalStorage/LocalStorage4";
 
+import ToDoList1 from "./ToDoList/ToDoList1";
+
 const App = () => (
   <div className="">
     {/* <LocalStorage1 />
     <LocalStorage2 /> */}
     {/* <LocalStorage3 /> */}
-    <LocalStorage4 />
+    {/* <LocalStorage4 /> */}
+    <ToDoList1 />
   </div>
 );
 
