6614933133138ccfe624731297cf908143d40759
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-31 21:27:09 +0700

    feat: chart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import ToDoList2 from "./ToDoListAnswers/ToDoList2";
 import ToDoList3 from "./ToDoListAnswers/ToDoList3";
 import ToDoList4 from "./ToDoListAnswers/ToDoList4";
 import ToDoList5 from "./ToDoListAnswers/ToDoList5";
+import Chart1 from "./ChartsAnswer/Chart1";
+import Chart2 from "./ChartsAnswer/Chart2";
+import Chart3 from "./ChartsAnswer/Chart3";
 
 const App = () => {
   return (
@@ -21,7 +24,10 @@ const App = () => {
       {/* <ToDoList2 /> */}
       {/* <ToDoList3 /> */}
       {/* <ToDoList4 /> */}
-      <ToDoList5 />
+      {/* <ToDoList5 /> */}
+      <Chart1 />
+      <Chart2 />
+      <Chart3 />
     </>
   );
 };
