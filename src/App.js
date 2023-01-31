3e3e7c35786f754e9a312f73cb5c30ed724df272
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-01-31 09:19:56 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
@@ -14,33 +13,6 @@ const App = () => (
     <Routes>
       <Route exact path="/" element={<Home />} />
        <Route exact path="/About" element={<About />} />
-=======
-import State3 from "./StatesAnswer/State3";
-import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
-import LocalStorage2 from "./LocalStorageAnswer/LocalStorage2";
-import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
-import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
-import ToDoList1 from "./ToDoListAnswers/ToDoList1";
-import ToDoList2 from "./ToDoListAnswers/ToDoList2";
-import ToDoList3 from "./ToDoListAnswers/ToDoList3";
-import ToDoList4 from "./ToDoListAnswers/ToDoList4";
-
-const App = () => {
-  return (
-    <>
-      {/* <State3 /> */}
-      {/* <LocalStorage1 /> */}
-      {/* <LocalStorage2 /> */}
-      <LocalStorage3 data1="bin" data2="hello"/>
-      {/* <LocalStorage4 /> */}
-      {/* <ToDoList1 /> */}
-      {/* <ToDoList2 /> */}
-      {/* <ToDoList3 /> */}
-      {/* <ToDoList4 /> */}
-    </>
-  );
-};
->>>>>>> b57e3ae62691892f9acbe897eefe3f6c0021ac7e
 
       <Route path="product">
         <Route path=":productId" element={<Products />} />
