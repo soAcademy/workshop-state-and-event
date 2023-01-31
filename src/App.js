69ff59fc87f64db1e0e06e2463d09e7cd44032e7
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-31 09:50:50 +0700

    feat:todolist workshop 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import LocalStorage1 from "./LocalStorage/LocalStorage1";
-import LocalStorage2 from "./LocalStorage/LocalStorage2";
-import LocalStorage3 from "./LocalStorage/LocalStorage3";
-import LocalStorage4 from "./LocalStorage/LocalStorage4";
+import ToDoList1 from "./ToDoList/ToDoList1";
 
 const App = () => {
   return (
     <>
-      {/* <LocalStorage1 /> */}
-      {/* <LocalStorage2 /> */}
-      {/* <LocalStorage3 /> */}
-      {/* <LocalStorage4 /> */}
-
-      <ToDoList4 />
+      <ToDoList1 />
     </>
   );
 };
