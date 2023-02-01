9711c03213e116bd0a649ed992c81a69808852eb
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-01 10:33:46 +0700

    feat: chart2 practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Chart1 from "./ChartsAnswer/Chart1";
-import Chart2 from "./ChartsAnswer/Chart2";
-import Chart3 from "./ChartsAnswer/Chart3";
+import Chart1 from "./Charts/Chart1";
+import Chart2 from "./Charts/Chart2";
+import Chart3 from "./Charts/Chart3";
 
 const App = () => {
   return (
     <>
-      <Chart1 />
-      {/* <Chart2 />
-      <Chart3 /> */}
+      {/* <Chart1 /> */}
+      <Chart2 />
+      {/* <Chart3 /> */}
     </>
   );
 };
--- src/Charts/Chart1.js
+++ src/Charts/Chart1.js
@@ -22,24 +22,32 @@ const Chart1 = () => {
     },
   ];
   const options = {
+    // แกน x
     xAxis: {
       type: "category",
       data: orders.map((order) => order.date),
       name: "วันที่",
     },
+    // แกน y
     yAxis: {
       type: "value",
       name: "มูลค่าสั่งซื้อ",
+      // max: "datamax",
+      // min: "datamin",
+      max: 28000,
+      min: 20000,
     },
     series: [
       {
         data: orders.map((order) => order.orderValue),
         type: "line",
         smooth: true,
+        lineStyle: { color: "d5ceeb", width: 5, type: "dashed" },
       },
     ],
     tooltip: {
       trigger: "axis",
+      // ตัว trigger ตอนเรา กดกราฟ
     },
   };
   return <ReactECharts option={options} />;
