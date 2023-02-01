3c454783604b2495b9d7d8c6435b6875014e87bf
jaiieth <midzty.rb128@gmail.com>
2023-02-01 09:40:52 +0700

    feat: Chart1

---
+++ src/AppChart.js
@@ -0,0 +1,11 @@
+import Chart1 from "./Charts/Chart1";
+
+const AppChart = () => {
+  return (
+    <div>
+      <Chart1 />
+    </div>
+  )
+}
+
+export default AppChart;
---
+++ src/Charts/Chart1.js
@@ -0,0 +1,46 @@
+import ReactECharts from "echarts-for-react";
+
+const Chart1 = () => {
+  const orders = [
+    {
+      date: "2022-01-13",
+      orderValue: 23000,
+    },
+    {
+      date: "2022-01-14",
+      orderValue: 25000,
+    },
+    {
+      date: "2022-01-15",
+      orderValue: 24000,
+    },
+    {
+      date: "2022-01-16",
+      orderValue: 23000,
+    },
+  ];
+  const options = {
+    xAxis: {
+      type: "category",
+      data: orders.map((e) => e.date),
+      name: "วันที่",
+    },
+    yAxis: {
+      type: "value",
+      name: "มูลค่าสั่งซื้อ",
+    },
+    series: [
+      {
+        data: orders.map((e) => e.orderValue),
+        type: "line",
+        smooth: true,
+      },
+    ],
+    tooltip: {
+      trigger: "axis",
+    },
+  };
+  return <ReactECharts option={options} />;
+};
+
+export default Chart1;
