748383aecd9d5c83919b08fd5a4c45afb2d24b8c
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-02-01 10:06:37 +0700

    Chart1

--- src/Charts/Chart1.js
+++ src/Charts/Chart1.js
@@ -1,49 +1,49 @@
 import ReactECharts from "echarts-for-react";
 
-const Chart1 = ()=>{
-
-const orders = [
-  {
-    date: "2022-01-13",
-    orderValue: 23000
-  },
-  {
-    date: "2022-01-14",
-    orderValue: 25000
-  },
-  {
-    date: "2022-01-15",
-    orderValue: 24000
-  },
-  {
-    date: "2022-01-16",
-    orderValue: 23000
-  }
-]
-const options = {
-  xAxis :{
-    type : "category",
-    data : orders.map(r=>r.date),
-    name : "วันที่"
-  },
-  yAxis :{
-    type : "value",
-    name : "มูลค่าสั่งซื้อ"
-  },
-  series : [
+const Chart1 = () => {
+  const orders = [
     {
-      data : orders.map(r=>r.orderValue),
-      type : "line",
-      smooth : true
-    }
-  ],
-    tooltip:{
-      trigger : "axis",
-    }
+      date: "2022-01-13",
+      orderValue: 23000,
+    },
+    {
+      date: "2022-01-14",
+      orderValue: 25000,
+    },
+    {
+      date: "2022-01-15",
+      orderValue: 24000,
+    },
+    {
+      date: "2022-01-16",
+      orderValue: 23000,
+    },
+  ];
+  const options = {
+    xAxis: {
+      type: "category",
+      data: orders.map((r) => r.date),
+      name: "วันที่",
+    },
+    yAxis: {
+      type: "value",
+      name: "มูลค่าสั่งซื้อ",
+    },
+    series: [
+      {
+        data: orders.map((r) => r.orderValue),
+        type: "line",
+        smooth: true,
+        itemStyle: { normal: { areaStyle: { type: "default" } } },
+        lineStyle: { color: "#ffbcd9" },
+        // darkMode: auto
+      },
+    ],
+    tooltip: {
+      trigger: "axis",
+    },
   };
-  
-  return <ReactECharts option= {options}/>
-  
 
+  return <ReactECharts option={options} />;
 };
 export default Chart1;
