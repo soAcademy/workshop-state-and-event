2629c0db9dd5b0e5ad76aeae622f7298d217b16b
Bond Rungrot <st122150@ait.asia>
2023-02-01 10:05:38 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import State3 from "./StatesAnswer/State3";
-import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
-import LocalStorage2 from "./LocalStorageAnswer/LocalStorage2";
-import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
-import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
-import ToDoList1 from "./ToDoListAnswers/ToDoList1";
-import ToDoList2 from "./ToDoListAnswers/ToDoList2";
-import ToDoList3 from "./ToDoListAnswers/ToDoList3";
-import ToDoList4 from "./ToDoListAnswers/ToDoList4";
-import ToDoList5 from "./ToDoListAnswers/ToDoList5";
-import Chart1 from "./ChartsAnswer/Chart1";
-import Chart2 from "./ChartsAnswer/Chart2";
-import Chart3 from "./ChartsAnswer/Chart3";
+// import State3 from "./StatesAnswer/State3";
+// import LocalStorage1 from "./LocalStorages/LocalStorage1";
+// import LocalStorage2 from "./LocalStorages/LocalStorage2";
+// import LocalStorage3 from "./LocalStorages/LocalStorage3";
+// import LocalStorage4 from "./LocalStorages/LocalStorage4";
+// import ToDoList1 from "./ToDoLists/ToDoList1";
+// import ToDoList2 from "./ToDoLists/ToDoList2";
+// import ToDoList3 from "./ToDoLists/ToDoList3";
+// import ToDoList4 from "./ToDoLists/ToDoList4";
+// import ToDoList5 from "./ToDoLists/ToDoList5";
+import Chart1 from "./Charts/Chart1";
+// import Chart2 from "./Charts/Chart2";
+// import Chart3 from "./Charts/Chart3";
 
 const App = () => {
   return (
@@ -26,8 +26,8 @@ const App = () => {
       {/* <ToDoList4 /> */}
       {/* <ToDoList5 /> */}
       <Chart1 />
-      <Chart2 />
-      <Chart3 />
+      {/* <Chart2 />
+      <Chart3 /> */}
     </>
   );
 };
