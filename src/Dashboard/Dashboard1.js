d3f2a26110f658cf8e69f48a8ddbe4d16fad163f
Phisit Siprasatthong <phisite@gmail.com>
2023-02-01 21:26:16 +0700

    feat: add year selector for Dashboard #1

--- src/Dashboard/Dashboard1.js
+++ src/Dashboard/Dashboard1.js
@@ -1,8 +1,9 @@
+import { useState } from "react";
 // import ReactECharts from "echarts-for-react";
 
 import thailandDeathCause from "./thailand-death-cause.json";
 
-const YEAR = 2557;
+const yearList = (tdc) => [...new Set(tdc.map((entry) => entry.year))].sort();
 
 const tdcByYear = (year) =>
   thailandDeathCause
@@ -30,8 +31,6 @@ const tdcByProvince = (tdc) =>
     }))
     .sort((a, b) => b.deathTotal - a.deathTotal);
 
-// console.log(tdcByProvince(tdcByYear(YEAR)));
-
 const codList = (tdc) => [...new Set(tdc.map((entry) => entry.causeOfDeath))];
 
 const tdcByCOD = (tdc) =>
@@ -44,9 +43,15 @@ const tdcByCOD = (tdc) =>
     }))
     .sort((a, b) => b.deathTotal - a.deathTotal);
 
-// console.log(tdcByCOD(tdcByYear(YEAR)));
-
 const Dashboard1 = () => {
+  const [year, setYear] = useState(Math.min(...yearList(thailandDeathCause)));
+
+  const handleYearSelect = (e) => {
+    e.preventDefault();
+
+    setYear(Number(e.target.options[e.target.options.selectedIndex].value));
+  };
+
   // const option = {
   //   title: {
   //     text: "Order value by date",
@@ -76,39 +81,60 @@ const Dashboard1 = () => {
   // };
 
   return (
-    <div className="flex flex-row gap-8 font-nstl">
+    <>
+      <div>
+        <label
+          htmlFor="year"
+          className="mb-2 block text-sm font-medium text-gray-900"
+        >
+          เลือกปี :
+        </label>
+        <select
+          id="year"
+          onChange={(e) => handleYearSelect(e)}
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
+        >
+          {yearList(thailandDeathCause).map((year, idx) => (
+            <option key={idx} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-row gap-8 font-nstl">
+        <div className="flex flex-col gap-4">
+          {tdcByProvince(tdcByYear(year)).map((cause, idx) => (
+            <div key={idx} className="flex flex-row gap-4">
+              <div>{cause.provinceName}</div>
+              <div>{cause.deathTotal.toLocaleString("TH")}</div>
+              <div>
+                {(
+                  (cause.deathTotal * 100) /
+                  tdcDeathTotal(tdcByYear(year))
+                ).toFixed(2)}
+                %
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col gap-4">
+          {tdcByCOD(tdcByYear(year)).map((cause, idx) => (
+            <div key={idx} className="flex flex-row gap-4">
+              <div>{cause.causeOfDeath}</div>
+              <div>{cause.deathTotal.toLocaleString("TH")}</div>
+              <div>
+                {(
+                  (cause.deathTotal * 100) /
+                  tdcDeathTotal(tdcByYear(year))
+                ).toFixed(2)}
+                %
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
       {/* <ReactECharts option={option} /> */}
-      <div className="flex flex-col gap-4">
-        {tdcByProvince(tdcByYear(YEAR)).map((cause, idx) => (
-          <div key={idx} className="flex flex-row gap-4">
-            <div>{cause.provinceName}</div>
-            <div>{cause.deathTotal.toLocaleString("TH")}</div>
-            <div>
-              {(
-                (cause.deathTotal * 100) /
-                tdcDeathTotal(tdcByYear(YEAR))
-              ).toFixed(2)}
-              %
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="flex flex-col gap-4">
-        {tdcByCOD(tdcByYear(YEAR)).map((cause, idx) => (
-          <div key={idx} className="flex flex-row gap-4">
-            <div>{cause.causeOfDeath}</div>
-            <div>{cause.deathTotal.toLocaleString("TH")}</div>
-            <div>
-              {(
-                (cause.deathTotal * 100) /
-                tdcDeathTotal(tdcByYear(YEAR))
-              ).toFixed(2)}
-              %
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
