57b7c398946fdcbf7ef3af2e9788e3c5ed67a8a9
Phisit Siprasatthong <phisite@gmail.com>
2023-02-01 13:07:59 +0700

    feat: add total death for DashBoard #1

--- src/Dashboard/Dashboard1.js
+++ src/Dashboard/Dashboard1.js
@@ -4,7 +4,11 @@ import thailandDeathCause from "./thailand-death-cause.json";
 
 const tdc2557 = thailandDeathCause
   .filter((entry) => entry.year === 2557 && entry.provinceKey === 19)
-  .sort((a, b) => b.deathMale + b.deathFemale - (a.deathMale + a.deathFemale));
+  .map((entry) => ({
+    ...entry,
+    deathTotal: entry.deathMale + entry.deathFemale,
+  }))
+  .sort((a, b) => b.deathTotal - a.deathTotal);
 
 // console.log(JSON.stringify(tdc2557, null, 2));
 
@@ -43,7 +47,7 @@ const Dashboard1 = () => {
       {tdc2557.map((cause) => (
         <>
           <div>{cause.causeOfDeath}</div>
-          <div>{cause.deathMale + cause.deathFemale}</div>
+          <div>{cause.deathTotal}</div>
         </>
       ))}
     </>
