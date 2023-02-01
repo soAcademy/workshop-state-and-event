92397774406674f579b9067d58281309ad1ad3cf
jaiieth <midzty.rb128@gmail.com>
2023-02-01 10:56:54 +0700

    feat: Chart3

--- src/AppChart.js
+++ src/AppChart.js
@@ -1,11 +1,13 @@
 import Chart1 from "./Charts/Chart1";
 import Chart2 from "./Charts/Chart2";
+import Chart3 from "./Charts/Chart3";
 
 const AppChart = () => {
   return (
     <div>
       <Chart1 />
       <Chart2 />
+      <Chart3 />
     </div>
   )
 }
