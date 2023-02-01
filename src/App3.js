77048b014e07cbe569864eb7c214d54830500e41
Firstweera <firstweera357@gmail.com>
2023-02-01 09:34:51 +0700

    feat: add App3

---
+++ src/App3.js
@@ -0,0 +1,36 @@
+// import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
+// import LocalStorage2 from "./LocalStorageAnswer/LocalStorage2";
+// import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
+// import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
+// import ToDoList1 from "./ToDoListAnswers/ToDoList1";
+// import ToDoList2 from "./ToDoListAnswers/ToDoList2";
+// import ToDoList3 from "./ToDoListAnswers/ToDoList3";
+// import ToDoList4 from "./ToDoListAnswers/ToDoList4";
+// import LocalStorage1 from "./LocalStorages/LocalStorage1";
+// import LocalStorage2 from "./LocalStorages/LocalStorage2";
+// import LocalStorage3 from "./LocalStorages/LocalStorage3";
+// import LocalStorage4 from "./LocalStorages/LocalStorage4";
+
+// import ToDoList1 from "./ToDoList/ToDoList1";
+// import ToDoList2 from "./ToDoList/ToDoList2";
+// import ToDoList3 from "./ToDoList/ToDoList3";
+// import ToDoList4 from "./ToDoList/ToDoList4";
+import ToDoList5 from "./ToDoList/ToDoList5";
+
+const App = () => {
+  return (
+    <>
+      {/* <LocalStorage1 /> */}
+      {/* <LocalStorage2 /> */}
+      {/* <LocalStorage3 /> */}
+      {/* <LocalStorage4 /> */}
+      {/* <ToDoList1 /> */}
+      {/* <ToDoList2 /> */}
+      {/* <ToDoList3 /> */}
+      {/* <ToDoList4 /> */}
+      <ToDoList5 />
+    </>
+  );
+};
+
+export default App;
