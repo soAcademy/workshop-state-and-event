9b852b11e253ddd0a65526182dcfe5ced77b561d
kunlanis <yoo.kunlanis@gmail.com>
2023-02-01 09:59:56 +0700

    feat:WS-Chart-1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ToDoList2 from "./ToDoListAnswers/ToDoList2";
 import ToDoList3 from "./ToDoListAnswers/ToDoList3";
 import ToDoList4 from "./ToDoListAnswers/ToDoList4";
 import ToDoList5 from "./ToDoListAnswers/ToDoList5";
-import Chart1 from "./ChartsAnswer/Chart1";
+import Chart1 from "./Charts/Chart1";
 import Chart2 from "./ChartsAnswer/Chart2";
 import Chart3 from "./ChartsAnswer/Chart3";
 
@@ -26,8 +26,8 @@ const App = () => {
       {/* <ToDoList4 /> */}
       {/* <ToDoList5 /> */}
       <Chart1 />
-      <Chart2 />
-      <Chart3 />
+      {/* <Chart2 /> */}
+      {/* <Chart3 /> */}
     </>
   );
 };
---
+++ src/Charts/Chart1.js
@@ -0,0 +1,47 @@
+import ReactECharts from "echarts-for-react";
+
+const Chart1 = () => {
+  const orders = [
+    {
+      date: "2022-01-13",
+      orderValue: 6000,
+    },
+    {
+      date: "2022-01-14",
+      orderValue: 25000,
+    },
+    {
+      date: "2022-01-15",
+      orderValue: 4000,
+    },
+    {
+      date: "2022-01-16",
+      orderValue: 23000,
+    },
+  ];
+
+  const options = {
+    xAxis: {
+      type: "category",
+      data: orders.map((order) => order.date),
+      name: "วันที่",
+    },
+    yAxis: {
+      type: "value",
+      name: "มูลค่าสั่งซื้อ",
+    },
+    series: [
+      {
+        data: orders.map((order) => order.orderValue),
+        type: "line",
+        smooth: true,
+      },
+    ],
+    tooltip: {
+      trigger: "item",
+    },
+  };
+  return <ReactECharts option={options} />;
+};
+
+export default Chart1;
