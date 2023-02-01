805c3626a47c0edb3c46710bb412ce2f62bb83fb
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-01 23:29:22 +0700

    feat: dashboard boeing practice

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Chart1 from "./Charts/Chart1";
 import Chart2 from "./Charts/Chart2";
 import Chart3 from "./Charts/Chart3";
 
+import ThailandDeathCause from "./Dashboard/ThailandDeathCause";
+
 const App = () => {
   return (
     <>
       {/* <Chart1 /> */}
       {/* <Chart2 /> */}
-      <Chart3 />
+      {/* <Chart3 /> */}
+      <ThailandDeathCause />
     </>
   );
 };
