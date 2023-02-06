513ca4c6f366b3b18a03bb1d28010f114a4fe154
kunlanis <yoo.kunlanis@gmail.com>
2023-02-06 13:56:40 +0700

    feat : quiz

--- src/App.js
+++ src/App.js
@@ -1,61 +1,12 @@
-<<<<<<< HEAD
-// import Accordion1 from "./Accordions/Accordion1";
-// import Accordion2 from "./Accordions/Accordion2";
-import Accordion3 from "./Accordions/Accordion3";
-=======
-import State3 from "./StatesAnswer/State3";
-import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
-import LocalStorage2 from "./LocalStorageAnswer/LocalStorage2";
-import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
-import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
-import ToDoList1 from "./ToDoListAnswers/ToDoList1";
-import ToDoList2 from "./ToDoListAnswers/ToDoList2";
-import ToDoList3 from "./ToDoListAnswers/ToDoList3";
-import ToDoList4 from "./ToDoListAnswers/ToDoList4";
-import ToDoList5 from "./ToDoListAnswers/ToDoList5";
-import Chart1 from "./ChartsAnswer/Chart1";
-import Chart2 from "./ChartsAnswer/Chart2";
-import Chart3 from "./ChartsAnswer/Chart3";
-import Accordian1 from "./AccordiansAnswer/Accordian1";
-import Accordian2 from "./AccordiansAnswer/Accordian2";
-import Accordian3 from "./AccordiansAnswer/Accordian3";
-import Trivia1 from "./TriviaAnswer/Trivia1";
-import Trivia2 from "./TriviaAnswer/Trivia2";
-import Trivia3 from "./TriviaAnswer/Trivia3";
-import Trivia4 from "./TriviaAnswer/Trivia4";
-import Trivia5 from "./TriviaAnswer/Trivia5";
->>>>>>> 05f25d1332fd3cca6fc52469943475e551dbee35
+// import Trivia1 from "./Trivia/Trivia1";
+
+import Trivia2 from "./Trivia/Trivia2";
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-      {/* <Accordion1 /> */}
-      {/* <Accordion2 /> */}
-      <Accordion3 />
-=======
-      {/* <State3 /> */}
-      {/* <LocalStorage1 /> */}
-      {/* <LocalStorage2 /> */}
-      {/* <LocalStorage3 data1="bin" data2="hello"/> */}
-      {/* <LocalStorage4 /> */}
-      {/* <ToDoList1 /> */}
-      {/* <ToDoList2 /> */}
-      {/* <ToDoList3 /> */}
-      {/* <ToDoList4 /> */}
-      {/* <ToDoList5 /> */}
-      {/* <Chart1 /> */}
-      {/* <Chart2 /> */}
-      {/* <Chart3 /> */}
-      {/* <Accordian1 /> */}
-      {/* <Accordian2 /> */}
-      {/* <Accordian3 /> */}
-      <Trivia1 />
-      {/* <Trivia2 /> */}
-      {/* <Trivia3 /> */}
-      {/* <Trivia4 /> */}
-      {/* <Trivia5 /> */}
->>>>>>> 05f25d1332fd3cca6fc52469943475e551dbee35
+      {/* <Trivia1 /> */}
+      <Trivia2/>
     </>
   );
 };
