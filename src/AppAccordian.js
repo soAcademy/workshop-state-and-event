0446b2110cff0d9f7c5489cf14105e85a8c869b8
jaiieth <midzty.rb128@gmail.com>
2023-02-06 10:37:15 +0700

    feat: Accordian2

--- src/AppAccordian.js
+++ src/AppAccordian.js
@@ -1,9 +1,11 @@
 import Accordian1 from "./Accordians/Accordian1";
+import Accordian2 from "./Accordians/Accordian2";
 
 const AppAccordian = () => {
   return (
     <div>
-      <Accordian1 />
+      {/* <Accordian1 /> */}
+      <Accordian2 />
     </div>
   );
 };
