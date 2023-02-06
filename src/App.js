08231027d5825a73783d5d5549d102be7bdc5c13
kunlanis <yoo.kunlanis@gmail.com>
2023-02-06 11:37:57 +0700

    feat:Accordion3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import Accordion1 from "./Accordions/Accordion1";
-import Accordion2 from "./Accordions/Accordion2";
-// import Accordion3 from "./Accordions/Accordion3";
+// import Accordion2 from "./Accordions/Accordion2";
+import Accordion3 from "./Accordions/Accordion3";
 
 const App = () => {
   return (
     <>
       {/* <Accordion1 /> */}
-      <Accordion2 />
-      {/* <Accordion3 /> */}
+      {/* <Accordion2 /> */}
+      <Accordion3 />
     </>
   );
 };
