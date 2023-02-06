a4230622986094df1ee8c396fd8d7faf7cc3e199
Firstweera <firstweera357@gmail.com>
2023-02-06 14:48:49 +0700

    feat: trivia5

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@
 // import Trivia1 from "./Trivia/Trivia1";
 // import Trivia2 from "./Trivia/Trivia2";
 // import Trivia3 from "./Trivia/Trivia3";
-import Trivia4 from "./Trivia/Trivia4";
+// import Trivia4 from "./Trivia/Trivia4";
+import Trivia5 from "./Trivia/Trivia5";
 
 const App = () => {
   return (
@@ -45,7 +46,8 @@ const App = () => {
       {/* <Trivia1 /> */}
       {/* <Trivia2 /> */}
       {/* <Trivia3 /> */}
-      <Trivia4 />
+      {/* <Trivia4 /> */}
+      <Trivia5 />
     </>
   );
 };
---
+++ src/Trivia/Trivia5.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+
+const Trivia4 = () => {
+  const quizes = [
+    {
+      question: "ซุปอะไรมีสารอาหารมากที่สุด",
+      answers: ["ซุปไก่สกัด", "ซุปหางวัว", "ซุปเปอร์มาเก็ต", "ซุปหูฉลาม"],
+      answer: 2,
+    },
+    {
+      question: "เพลง แปดโมงเช้าวันอังคาร ร้องยังไง",
+      answers: [
+        "แปดโมงเช้าวันอังคาร",
+        "รู้บ้างไหมว่าจะไป มันไม่จบแค่นั้น",
+        "ประเทศไทยรวมเลือดเนื้อชาติเชื้อไทย…",
+        "ไม่อยากรู้วันเวลา เช้าขึ้นมาไม่อยากเจอ",
+      ],
+      answer: 2,
+    },
+    {
+      question: "จะแบ่งกระทิงแดง 5 ขวดให้คน 2 คนเท่าๆ กันต้องทำไง",
+      answers: [
+        "แบ่งได้ คนละ 2 ขวด",
+        "แบ่งได้ คนละ 2.5 ขวด",
+        "แบ่งได้ คนละ 1 ขวด",
+        "แบ่งไม่ได้",
+      ],
+      answer: 0,
+    },
+    {
+      question: "ก่อนจะถึงประเทศอาหลับ ต้องถึงประเทศอะไรก่อน",
+      answers: ["เลบานอน", "อิสราเอล", "นิวซีแลนด์", "ออสเตเรีย"],
+      answer: 0,
+    },
+    {
+      question: "เกาะ อะไรมีเสาไฟฟ้า เยอะที่สุด",
+      answers: ["เกาะสีชัง", "เกาะกลางถนน", "เกาะช้าง", "เกาะเกร็ด"],
+      answer: 1,
+    },
+  ];
+
+  // const [quizId, setQuizId] = useState(1);
+  const [quizId, setQuizId] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const checkAnswer = (id, answer) => {
+    // console.log(quizId, answer);
+    const newScore = quizes[id].answer === answer ? 1 : 0;
+    const totalScore = score + newScore;
+    setScore(totalScore);
+    setQuizId(id + 1);
+  };
+  console.log(score);
+  return (
+    <div>
+      {quizId < quizes.length && (
+        <div className="p-4">
+          <div className="text-center mb-4 text-xl">
+            <p>
+              ข้อ {quizId + 1}/{quizes.length}
+            </p>
+          </div>
+          <div className="text-center text-lg mb-4">
+            <p>คะแนน : {score}</p>
+          </div>
+          <div className="text-lg mb-4">
+            <p className="mb-2">คำถาม :</p>
+            <div>{quizes[quizId].question}</div>
+          </div>
+          <div className="grid gap-4 grid-cols-2">
+            {quizes[quizId].answers.map((r, idx) => (
+              <button
+                key={idx}
+                className="bg-cyan-400 h-12 rounded active:bg-cyan-200"
+                onClick={() => checkAnswer(quizId, idx)}
+              >
+                {r}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+      {quizId >= quizes.length && (
+        <div className="w-full h-screen fixed flex bg-gray-500/30 backdrop-blur-sm">
+          <div className="flex flex-col m-auto bg-white p-5 w-3/5 rounded-lg text-center">
+            <p className="text-xl font-semibold">สรุปคะแนน : {score}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Trivia4;
