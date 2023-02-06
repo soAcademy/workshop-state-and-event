c6b315f148ea0efc35b346896e1251ca5222948b
Firstweera <firstweera357@gmail.com>
2023-02-06 10:16:54 +0700

    feat: Accor1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import State3 from "./StatesAnswer/State3";
-import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
-import LocalStorage2 from "./LocalStorageAnswer/LocalStorage2";
-import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
-import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
-import ToDoList1 from "./ToDoListAnswers/ToDoList1";
-import ToDoList2 from "./ToDoListAnswers/ToDoList2";
-import ToDoList3 from "./ToDoListAnswers/ToDoList3";
-import ToDoList4 from "./ToDoListAnswers/ToDoList4";
-import ToDoList5 from "./ToDoListAnswers/ToDoList5";
-import Chart1 from "./ChartsAnswer/Chart1";
-import Chart2 from "./ChartsAnswer/Chart2";
-import Chart3 from "./ChartsAnswer/Chart3";
-import Accordian1 from "./AccordiansAnswer/Accordian1";
-import Accordian2 from "./AccordiansAnswer/Accordian2";
-import Accordian3 from "./AccordiansAnswer/Accordian3";
+// import State3 from "./StatesAnswer/State3";
+// import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
+// import LocalStorage2 from "./LocalStorageAnswer/LocalStorage2";
+// import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
+// import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
+// import ToDoList1 from "./ToDoListAnswers/ToDoList1";
+// import ToDoList2 from "./ToDoListAnswers/ToDoList2";
+// import ToDoList3 from "./ToDoListAnswers/ToDoList3";
+// import ToDoList4 from "./ToDoListAnswers/ToDoList4";
+// import ToDoList5 from "./ToDoListAnswers/ToDoList5";
+// import Chart1 from "./ChartsAnswer/Chart1";
+// import Chart2 from "./ChartsAnswer/Chart2";
+// import Chart3 from "./ChartsAnswer/Chart3";
+// import Accordian1 from "./AccordiansAnswer/Accordian1";
+// import Accordian2 from "./AccordiansAnswer/Accordian2";
+// import Accordian3 from "./AccordiansAnswer/Accordian3";
+
+import Accordian1 from "./Accordians/Accordian1";
 
 const App = () => {
   return (
@@ -31,8 +33,8 @@ const App = () => {
       {/* <Chart1 /> */}
       {/* <Chart2 /> */}
       {/* <Chart3 /> */}
-      {/* <Accordian1 /> */}
-      <Accordian2 />
+      <Accordian1 />
+      {/* <Accordian2 /> */}
       {/* <Accordian3 /> */}
     </>
   );
