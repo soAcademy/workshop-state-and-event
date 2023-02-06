a514ae6b0bb2e8dd80f55e7a9c5448e197ed1b67
Poom <poomhan@gmail.com>
2023-02-06 09:58:26 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Chart1 from "./Charts/Chart1";
 import Chart2 from "./Charts/Chart2";
 import Chart3 from "./Charts/Chart3";
+import Dashboard from "./Dashboard/Dashboard";
 const App = () => {
   return <>
   {/* <Chart1/> */}
   {/* <Chart2/> */}
-  <Chart3/>
+  {/* <Chart3/> */}
+  <Dashboard/>
   </>;
 };
 
---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,47 @@
+import ReactECharts from "echarts-for-react";
+import jsonData from "./thailand-death-cause.json";
+
+// ...new Set เพื่อแยกสาเหตุการตายที่ไม่ซ้ำ
+// filter ข้อมูล เพื่อหาคนตายด้วยโรคนั้น
+// reduce หายอดรวมสาเหตุการตาย แยก ช ฐ
+// ทำไม .map อยู่ครอบทั้งหมด
+// filter ค่า uniqueDeathCause ที่เป็น array ได้หรือไม่
+
+const Dashboard = () => {
+  const data = jsonData.thailand;
+  const uniqueDeathCause = [...new Set(data.map((r) => r.causeOfDeath))];
+  // console.log("uniqueDeathCause", uniqueDeathCause);
+
+  // const uniqueProvice = [...new Set(data.map((r) => r.provinceName))];
+  // // console.log("uniqueProvice", uniqueProvice);
+
+  // const deathByPrice = uniqueProvice.map((d)=> {
+  //   const filterData1 = data.filter((r) => r.provinceName === d );
+  //   // console.log(filterData1)
+  // })
+
+  const deathByCause = uniqueDeathCause.map((d) => {
+    const filterData = data.filter((r) => r.causeOfDeath === d);
+    // console.log("filterData 1", filterData);
+
+    const sumOfDeaths = filterData.reduce(
+      (acc, r) => {
+        acc.deathFemale =+ r.deathFemale;
+        acc.deathMale =+ r.deathMale;
+        return acc;
+      },
+      { deathFemale: 0, deathMale: 0 }
+    );
+    // console.log("sumOfDeaths", sumOfDeaths);
+
+    return {
+      causeOfDeath: d,
+      deathFemale: sumOfDeaths.deathFemale,
+      deathMale: sumOfDeaths.deathMale,
+      death: sumOfDeaths.deathFemale + sumOfDeaths.deathMale,
+    };
+  });
+  console.log(deathByCause);
+};
+
+export default Dashboard;
