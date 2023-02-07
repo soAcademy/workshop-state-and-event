0b494cc3e364dfd5c9fde050c670d9d28642cf54
Your name <teak.yuktanand@gmail.com>
2023-02-07 10:02:01 +0700

    feat: add Zipcode2

--- src/zipcodes/Zipcode1.js
+++ src/zipcodes/Zipcode1.js
@@ -1,15 +1,17 @@
+import thailandZipcodeData from "./thailand-zipcode.json"
+
 const Zipcode1 = () => {
-  const provinces = ["กรุงเทพมหานคร", "นนทบุรี", "สุโขทัย", "แพร่"];
+  const provinces = [...new Set(thailandZipcodeData.map((r) => r.province))] //use ...new Set to only get unique provinces
   return (
     <div>
     <div className="text-center">
-      <h1 className="font-bold my-2 text-xl text-sky-300">Zipcode Search</h1>
+      <h1 className="font-bold my-2 text-2xl text-sky-300">Zipcode Search</h1>
       
         <input type="text" placeholder="Search Zipcode" className="border border-4 my-2 border-sky-500"/>
       
     </div>
     <div className="bg-sky-300 rounded-lg w-2/3 mx-auto mt-2 p-3">
-      <h2 className="text-left">Select Province</h2>
+      <h2 className="text-left font-bold text-xl">Select Province</h2>
       <div className="grid grid-cols-4 gap-2 text-center mt-2">
       {provinces.map((province) => (
           <div>
