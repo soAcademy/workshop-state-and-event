837a5f2ed1b39e04473143dbc7bca0ab56063604
kunlanis <yoo.kunlanis@gmail.com>
2023-02-07 10:11:05 +0700

    WS-ZIP-2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Zipcode1 from "./Zipcode/Zipcode1";
-// import Zipcode2 from "./Zipcode/Zipcode2";
+// import Zipcode1 from "./Zipcode/Zipcode1";
+import Zipcode2 from "./Zipcode/Zipcode2";
 // import Zipcode3 from "./Zipcode/Zipcode3";
 // import Zipcode4 from "./Zipcode/Zipcode4";
 // import Zipcode5 from "./Zipcode/Zipcode5";
@@ -10,8 +10,8 @@ import Zipcode1 from "./Zipcode/Zipcode1";
 const App = () => {
   return (
     <>
-      <Zipcode1 />
-      {/* <Zipcode2 /> */}
+      {/* <Zipcode1 /> */}
+      <Zipcode2 />
       {/* <Zipcode3 /> */}
       {/* <Zipcode4 /> */}
       {/* <Zipcode5 /> */}
