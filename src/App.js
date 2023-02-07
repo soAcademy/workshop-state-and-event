da06e8da0d599ea194b60beb152ed5ebba8dc149
kunlanis <yoo.kunlanis@gmail.com>
2023-02-07 09:53:42 +0700

    WS-ZIP-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import Zipcode1 from "./ZipcodeAnswer/Zipcode1";
-import Zipcode2 from "./ZipcodeAnswer/Zipcode2";
-import Zipcode3 from "./ZipcodeAnswer/Zipcode3";
-import Zipcode4 from "./ZipcodeAnswer/Zipcode4";
-import Zipcode5 from "./ZipcodeAnswer/Zipcode5";
-import Zipcode6 from "./ZipcodeAnswer/Zipcode6";
-import Zipcode7 from "./ZipcodeAnswer/Zipcode7";
-import Zipcode8 from "./ZipcodeAnswer/Zipcode8";
+import Zipcode1 from "./Zipcode/Zipcode1";
+// import Zipcode2 from "./Zipcode/Zipcode2";
+// import Zipcode3 from "./Zipcode/Zipcode3";
+// import Zipcode4 from "./Zipcode/Zipcode4";
+// import Zipcode5 from "./Zipcode/Zipcode5";
+// import Zipcode6 from "./Zipcode/Zipcode6";
+// import Zipcode7 from "./Zipcode/Zipcode7";
+// import Zipcode8 from "./Zipcode/Zipcode8";
 
 const App = () => {
   return (
     <>
-      {/* <Zipcode1 /> */}
+      <Zipcode1 />
       {/* <Zipcode2 /> */}
       {/* <Zipcode3 /> */}
       {/* <Zipcode4 /> */}
       {/* <Zipcode5 /> */}
       {/* <Zipcode6 /> */}
       {/* <Zipcode7 /> */}
-      <Zipcode8 />
+      {/* <Zipcode8 /> */}
     </>
   );
 };
