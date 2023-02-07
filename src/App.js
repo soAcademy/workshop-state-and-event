3ace3f906c49e63365ce7554b7b99509d278838d
Poom <poomhan@gmail.com>
2023-02-07 13:32:13 +0700

    6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@
 // import Zipcode1 from "./Zipcode/Zipcode1";
 // import Zipcode2 from "./Zipcode/Zipcode2";
 // import Zipcode3 from "./Zipcode/Zipcode3";
-import Zipcode4 from "./Zipcode/Zipcode4";
+// import Zipcode4 from "./Zipcode/Zipcode4";
+// import Zipcode5 from "./Zipcode/Zipcode5";
+import Zipcode6 from "./Zipcode/Zipcode6";
 
 
 const App = () => {
@@ -36,7 +38,9 @@ const App = () => {
       {/* <Zipcode1 /> */}
       {/* <Zipcode2/> */}
       {/* <Zipcode3/> */}
-      <Zipcode4/>
+      {/* <Zipcode4/> */}
+      {/* <Zipcode5/> */}
+      <Zipcode6/>
     </>
   );
 };
