a1a56563e6ef2f1fbfd094ffc1b46934d1c6195f
Phisit Siprasatthong <phisite@gmail.com>
2023-02-07 13:18:46 +0700

    feat: add solution for Zipcode #6

--- src/App.js
+++ src/App.js
@@ -87,8 +87,8 @@
 // import Zipcode2 from "./Zipcode/Zipcode2";
 // import Zipcode3 from "./Zipcode/Zipcode3";
 // import Zipcode4 from "./Zipcode/Zipcode4";
-import Zipcode5 from "./Zipcode/Zipcode5";
-// import Zipcode6 from "./Zipcode/Zipcode6";
+// import Zipcode5 from "./Zipcode/Zipcode5";
+import Zipcode6 from "./Zipcode/Zipcode6";
 // import Zipcode7 from "./Zipcode/Zipcode7";
 // import Zipcode8 from "./Zipcode/Zipcode8";
 
@@ -147,8 +147,8 @@ const App = () => (
     {/* <Zipcode2 /> */}
     {/* <Zipcode3 /> */}
     {/* <Zipcode4 /> */}
-    <Zipcode5 />
-    {/* <Zipcode6 /> */}
+    {/* <Zipcode5 /> */}
+    <Zipcode6 />
     {/* <Zipcode7 /> */}
     {/* <Zipcode8 /> */}
   </div>
