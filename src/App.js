789def1ab226e531ce060fe07d2b67e724c4fec8
Your name01~ <poovareskkc@gmail.com>
2023-02-07 11:15:32 +0700

    ZC4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Zipcode1 from './Zipcode/Zipcode1';
 import Zipcode2 from './Zipcode/Zipcode2';
 import Zipcode3 from './Zipcode/Zipcode3';
-
+import Zipcode4 from './Zipcode/Zipcode4';
 const App = () => {
   return (
     <>
       
       {/* <Zipcode1 /> */}
       {/* <Zipcode2 /> */}
-      <Zipcode3 />
+      {/* <Zipcode3 /> */}
+      <Zipcode4 />
 
     </>
   );
---
+++ src/Zipcode/Zipcode4.js
@@ -0,0 +1,77 @@
+import ThailandZipcodeData from "./thailand-zipcode.json";
+
+
+  const ZipcodeData = ({ provinces }) => (
+  <>  
+  <h1 className="text-2xl pt-3 font-bold underline">ค้นหารหัสไปรษณีย์</h1>
+      <div>
+      <input
+          type="text"
+          className="border-4 border-sky-400 rounded-lg p-4 mt-6 w-2/3"
+          placeholder="ค้นหา ตำบล อำเภอ จังหวัด รหัสไปรษณีย์"
+        />
+      </div>
+      <div className="w-3/3 mx-auto bg-blue-100 p-2 mt-6 rounded-lg text-left">
+        <h2 className="text-xl mt-2 font-bold">เลือกจังหวัด</h2>
+        <div className="grid grid-cols-4 mt-4">
+        {provinces.map((province) => (
+            <div>{province}</div>
+          ))}
+        </div>
+      </div>
+  </>
+  );
+
+const ZipcodeProvince = () => (
+  <>
+  <h1 className="pt-4 text-2xl font-bold underline" >
+    รหัสไปรษณีย์ในจังหวัด กรุงเทพมหานคร
+  </h1>
+  <div>
+    <input 
+    type="text"
+    className="border-2 border-sky-400 rounded-lg p-2 mt-4 w-2/3"
+    placeholder="ค้นหา ตำบล อำเภอ จังหวัด รหัสไปรษณีย์"
+    />
+  </div>
+  <div className="p-4 mt-8 w-2/3 mx-auto bg-blue-100 rounded-lg text-left " >
+  <table className="w-full" >
+    <thead>
+      <tr className="text-center font-bold border-collapse">
+        <th className="p-2 border border-sky-400" >#</th>
+        <th className="p-2 border border-sky-400" >อำเภอ/เขต</th>
+        <th className="p-2 border border-sky-400" >รหัสไปรษณีย์</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td className="p-2 border border-sky-400 text-center " >1</td>
+        <td className="p-2 border border-sky-400 ">เขตคลองสาน</td>
+        <td className="p-2 border border-sky-400 text-center " >10600</td>
+      </tr>
+    </tbody>
+
+  </table>
+  </div>
+  </>
+)
+    
+  
+  
+      
+    
+
+const Zipcode1 = () => {
+  const provinces = [...new Set(ThailandZipcodeData.map((r) => r.province))];        
+
+  return (
+    <div className="w-full text-center">
+      {/* <ZipcodeData provinces={provinces} /> */}
+      <ZipcodeProvince />
+    </div>
+
+
+  );
+
+};
+export default Zipcode1;
