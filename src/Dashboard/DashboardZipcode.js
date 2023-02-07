f83e228941d784b5bec598e49841b6554c90615d
jaiieth <midzty.rb128@gmail.com>
2023-02-07 12:02:41 +0700

    fix: page appearance

--- src/Dashboard/DashboardZipcode.js
+++ src/Dashboard/DashboardZipcode.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const DashboardZipcode = () => {
   const uniqueProvince = [...new Set(zipcodeData.map((e) => e.province))];
   return (
-    <div className="flex flex-col justify-center m-10 font-kanit absolute top-1/3 w-full z-10">
+    <div className="flex flex-col justify-center m-10 font-kanit absolute top-1/4 w-full z-10">
       <div className="mt-10">
         <p>เลือกจังหวัด</p>
         <div className="grid grid-cols-4">
