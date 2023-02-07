5615931dbd726f9fe5af9e92ec738128eed53851
Firstweera <firstweera357@gmail.com>
2023-02-07 11:55:24 +0700

    feat: update new Map

--- src/Zipcode/Zipcode5.js
+++ src/Zipcode/Zipcode5.js
@@ -1,30 +1,30 @@
 import React from "react";
 import thailandZipCode from "../ZipcodeAnswer/thailand-zipcode.json";
 
-const ZipCodeHome = ({ provinces }) => {
-  return (
-    <div>
-      <div className="w-full text-center mt-5">
-        <p className="font-semibold text-xl">ค้นหารหัสไปรษณีย์</p>
-      </div>
-      <div className="flex justify-center mt-5">
-        <input
-          type="text"
-          className="border-2 border-gray-400 w-2/3 rounded"
-          placeholder="ค้นหา ตำบล อำเภอ จังหวัด รหัสไปรษณีย์"
-        />
-      </div>
-      <div className="mx-auto mt-14 py-2 px-4 w-4/5 bg-gradient-to-b from-red-100 to-blue-100 rounded text-left">
-        <p className="text-lg">เลือกจังหวัด :</p>
-        <div className="grid gap-4 grid-cols-4 pl-4 mt-4">
-          {provinces.map((r) => (
-            <div>{r}</div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+// const ZipCodeHome = ({ provinces }) => {
+//   return (
+//     <div>
+//       <div className="w-full text-center mt-5">
+//         <p className="font-semibold text-xl">ค้นหารหัสไปรษณีย์</p>
+//       </div>
+//       <div className="flex justify-center mt-5">
+//         <input
+//           type="text"
+//           className="border-2 border-gray-400 w-2/3 rounded"
+//           placeholder="ค้นหา ตำบล อำเภอ จังหวัด รหัสไปรษณีย์"
+//         />
+//       </div>
+//       <div className="mx-auto mt-14 py-2 px-4 w-4/5 bg-gradient-to-b from-red-100 to-blue-100 rounded text-left">
+//         <p className="text-lg">เลือกจังหวัด :</p>
+//         <div className="grid gap-4 grid-cols-4 pl-4 mt-4">
+//           {provinces.map((r) => (
+//             <div>{r}</div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 const ZipCodeProvince = (props) => {
   return (
@@ -65,28 +65,30 @@ const ZipCodeProvince = (props) => {
 };
 
 const ZipCode5 = () => {
-  const provinces = [...new Set(thailandZipCode.map((r) => r.province))];
+  // const provinces = [...new Set(thailandZipCode.map((r) => r.province))];
   // console.log(provinces);
   const province = "เชียงใหม่";
-  // const filterDistricts = thailandZipCode.filter(
-  //   (r) => r.province === province
-  // );
-  // console.log("filter", filterDistricts);
-  // const mapDistrict = filterDistricts.map((r) => {
+  const filterDistricts = thailandZipCode.filter(
+    (r) => r.province === province
+  );
+  console.log("filter", filterDistricts);
+  // const mapDistrict = filterDistricts.map((r) => { //ไม่ต้อง map แยก
   //   return { p: r.province, d: r.district };
   // });
   // console.log("map", mapDistrict);
-  // const newMapDistrict = [...new Map(mapDistrict).values()]; // ยังทำไม่ได้
-  // console.log("newMap", newMapDistrict);
+  const newMapDistrict = [
+    ...new Map(filterDistricts.map((r) => [r.district, r])).values(),
+  ]; // ยังทำไม่ได้---> ไม่ต้อง map แล้วมา map ในนี้ได้เลย
+  console.log("newMap", newMapDistrict);
 
-  const districts = [
-    ...new Map(
-      thailandZipCode
-        .filter((r) => r.province === province)
-        .map((r) => [r.district, r])
-    ).values(),
-  ];
-  console.log(districts);
+  // const districts = [ --> Answer Version
+  //   ...new Map(
+  //     thailandZipCode
+  //       .filter((r) => r.province === province)
+  //       .map((r) => [r.district, r])
+  //   ).values(),
+  // ];
+  // console.log(districts);
 
   return (
     <div>
@@ -94,7 +96,7 @@ const ZipCode5 = () => {
         {/* <ZipCodeHome provinces={provinces} /> */}
       </div>
       <div>
-        <ZipCodeProvince province={province} districts={districts} />
+        <ZipCodeProvince province={province} districts={newMapDistrict} />
       </div>
     </div>
   );
