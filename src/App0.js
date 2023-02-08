5dcb60b72717efe847c16f7775b70ae38f579b9a
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-02-08 10:29:18 +0700

    DeathCause2

--- src/App0.js
+++ src/App0.js
@@ -2,12 +2,12 @@
 // import Dashboard from "./Dashboard/Dashboard";
 // import Accordian3 from "./Accordian/Accordian3"
 // import Accordian1 from "./Playground/accordian";
-import DeathCause1 from "./DeathCause/DeathCause1_Dew";
+import DeathCause2 from "./DeathCause/DeathCause2_Dew";
 // import Zipcode8 from "./Zipcode/Zip8_Autosuggest";
 const App0 = () => {
   return (
     <div>
-      <DeathCause1/>
+      <DeathCause2/>
       {/* <Zipcode8/> */}
      
     </div>
