990658a33bfeacffe401d8f4c6ad5e5af0d01e09
Phisit Siprasatthong <phisite@gmail.com>
2023-02-08 22:41:22 +0700

    feat: add finished solution for DeathCause #8 (add cause of death selectors)

--- src/DeathCause/DeathCause8.js
+++ src/DeathCause/DeathCause8.js
@@ -8,7 +8,9 @@ const DeathsByCauseTable = ({
   currentYear,
   setSelectedCause,
 }) => {
-  const handleCauseButtonClick = () => {};
+  const handleCauseButtonClick = (cause) => {
+    setSelectedCause(cause);
+  };
 
   return (
     <div>
@@ -59,21 +61,25 @@ const DeathsByCauseTable = ({
 };
 
 const DeathsByProvinceTable = ({
-  totalDeath,
   deathsByProvince,
   currentYear,
+  selectedCause,
 }) => {
   return (
     <div>
       <h2 className="mb-2 text-xl">
-        จำนวนผู้เสียชีวิตปี {currentYear} แยกตามจังหวัด
+        จำนวนผู้เสียชีวิตปี {currentYear}{" "}
+        {selectedCause ? `ด้วยสาเหตุ${selectedCause} ` : ""}แยกตามจังหวัด
       </h2>
       <table className="table-auto text-left font-nstl text-sm text-gray-900">
         <tbody>
           <tr className="border-b bg-white">
             <td className="py-2 pr-2">ทั้งหมด</td>
             <td className="px-2 py-2 text-right">
-              {totalDeath.toLocaleString("TH")}
+              {/* {totalDeath.toLocaleString("TH")} ={" "} */}
+              {deathsByProvince
+                .reduce((acc, death) => acc + death.death, 0)
+                .toLocaleString("TH")}
             </td>
             <td className="py-2 pl-2 text-right">100.00%</td>
           </tr>
@@ -86,7 +92,10 @@ const DeathsByProvinceTable = ({
                 {deathByProvince.death.toLocaleString("TH")}
               </td>
               <td className="py-2 pl-2 text-right">
-                {((deathByProvince.death * 100) / totalDeath)
+                {(
+                  (deathByProvince.death * 100) /
+                  deathsByProvince.reduce((acc, death) => acc + death.death, 0)
+                )
                   .toFixed(2)
                   .toLocaleString("TH")}
                 %
@@ -388,7 +397,7 @@ const DeathCause8 = () => {
     setDeathsByProvince(_deathsByProvince);
     setOptionForDeathTrend(_optionForDeathTrend);
     setOptionForDeathsByGender(_optionForDeathsByGender);
-  }, [currentYear]);
+  }, [currentYear, selectedCause]);
 
   const handleYearSelect = (e) => {
     e.preventDefault();
@@ -396,6 +405,7 @@ const DeathCause8 = () => {
     setCurrentYear(
       Number(e.target.options[e.target.options.selectedIndex].value)
     );
+    setSelectedCause(undefined);
   };
 
   return (
@@ -433,7 +443,6 @@ const DeathCause8 = () => {
           setSelectedCause={setSelectedCause}
         />
         <DeathsByProvinceTable
-          totalDeath={totalDeath}
           deathsByProvince={deathsByProvince}
           currentYear={currentYear}
           selectedCause={selectedCause}
