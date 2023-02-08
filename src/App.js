2d537acb10c06678b7757fde2926630b54c36ef5
Phisit Siprasatthong <phisite@gmail.com>
2023-02-08 14:31:14 +0700

    feat: add solution for DeathCause #7

--- src/App.js
+++ src/App.js
@@ -97,8 +97,8 @@
 // import DeathCause3 from "./DeathCause/DeathCause3";
 // import DeathCause4 from "./DeathCause/DeathCause4";
 // import DeathCause5 from "./DeathCause/DeathCause5";
-import DeathCause6 from "./DeathCause/DeathCause6";
-// import DeathCause7 from "./DeathCause/DeathCause7";
+// import DeathCause6 from "./DeathCause/DeathCause6";
+import DeathCause7 from "./DeathCause/DeathCause7";
 // import DeathCause8 from "./DeathCause/DeathCause8";
 
 // import Dashboard1 from "./Dashboard/Dashboard1";
@@ -165,8 +165,8 @@ const App = () => (
     {/* <DeathCause3 /> */}
     {/* <DeathCause4 /> */}
     {/* <DeathCause5 /> */}
-    <DeathCause6 />
-    {/* <DeathCause7 /> */}
+    {/* <DeathCause6 /> */}
+    <DeathCause7 />
     {/* <DeathCause8 /> */}
   </div>
 );
