28964055d25c15b6b0cdcfea7c581caed13ae7db
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-08 10:24:39 +0700

    feat:DeathCause 2

---
+++ src/DeathCause/DeathCause2.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+const DeathCause2 = () => {
+  const DeathByCause = () => (
+    <div className="bg-yellow-400 w-1/3 p-4">
+      <div className="font-bold mb-2">สาเหตุการเสียชีวิต</div>
+      <table>
+        <tbody>
+          <tr>
+            <td>ทั้งหมด</td>
+            <td>408,000</td>
+            <td>100%</td>
+          </tr>
+          <tr>
+            <td>วัณโรคทุกชนิด</td>
+            <td>189,000</td>
+            <td>3.48%</td>
+          </tr>
+          <tr>
+            <td>เบาหวาน</td>
+            <td>130,000</td>
+            <td>2.48%</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+
+  const DeathByProvince = () => (
+    <div className="bg-sky-400 w-1/3 p-4">
+      <div className="font-bold mb-2">จำนวนผู้เสียชีวิตแยกตามจังหวัด</div>
+      <table>
+        <tbody>
+          <tr>
+            <td>ทั้งหมด</td>
+            <td>408,000 คน</td>
+            <td>100%</td>
+          </tr>
+          <tr>
+            <td>กรุงเทพมหานคร</td>
+            <td>189,000 คน</td>
+            <td>3.48%</td>
+          </tr>
+          <tr>
+            <td>เชียงใหม่</td>
+            <td>109,000 คน</td>
+            <td>1.48%</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+
+  const DeathChart = () => (
+    <div className="bg-green-400 w-1/3 p-4">
+      <div className="font-bold mb-2">จำนวนผู้เสียชีวิตแยกตามจังหวัด</div>
+      <table></table>
+    </div>
+  );
+
+  return (
+    <div className="p-4">
+      <h1 className="font-bold text-xl">
+        จำนวนผู้เสียชีวิต สาเหตุ และอัตราการตาย ปี 2554 - 2559
+      </h1>
+      <div className="mt-4">ปีพ.ศ. 2559</div>
+      <div className="flex space-x-4 mt-4">
+        <DeathByCause />
+        <DeathByProvince />
+        <DeathChart />
+      </div>
+    </div>
+  );
+};
+
+export default DeathCause2;
