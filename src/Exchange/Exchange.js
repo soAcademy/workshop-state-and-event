6d0abd0cf33f1c9fdd9109af1f2beb0c6e198eaf
kunlanis <yoo.kunlanis@gmail.com>
2023-02-13 16:12:48 +0700

    exchange rate

--- src/Exchange/Exchange.js
+++ src/Exchange/Exchange.js
@@ -3,75 +3,178 @@ import ExchangeData from "./ExchangeData.json";
 import ExchangeStatistic from "./ExchangeStatistic.json";
 import { BsArrowLeftRight } from "react-icons/bs";
 import ExchangeChart from "./ExchangeChart.json";
+import ReactECharts from "echarts-for-react";
+import axios from "axios";
 
-const Exchange = () => {
+const useFetchExchangeRate = ({ authToken }) => {
   const [exchangeRates, setExchangeRates] = useState();
   const [currencyLists, setCurrencyLists] = useState([]);
+  console.log("useFetchExchangeRate");
+  useEffect(() => {
+    axios({
+      url: `https://anyorigin-iinykauowa-uc.a.run.app/?url=${encodeURIComponent(
+        "https://www.xe.com/api/protected/midmarket-converter/"
+      )}`,
+      headers: {
+        Authorization: authToken,
+      },
+    }).then((res) => {
+      console.log("useFetchExchangeRate", res.data);
+      const _exchangeRates = res.data;
+      const _currencyLists = Object.keys(_exchangeRates.rates);
+      console.log("currencyLists", _currencyLists);
+      setExchangeRates(_exchangeRates);
+      setCurrencyLists(_currencyLists);
+    });
+  }, []);
+
+  return { exchangeRates, currencyLists };
+};
+
+const useConvertExchangeRate = ({ exchangeRates }) => {
   const [fromCurrency, setFromCurrency] = useState("THB");
   const [toCurrency, setToCurrency] = useState("USD");
   const [amount, setAmount] = useState(1);
   const [amountConvert, setAmountConvert] = useState(0);
   const [fromCurrencyRate, setFromCurrencyRate] = useState();
   const [toCurrencyRate, setToCurrencyRate] = useState();
-  const [exchangeStatistic, setExchangeRatesStatistic] = useState();
-  const [chartOption, setChartOption] = useState({});
-
-  useEffect(() => {
-    const _exchangeRates = ExchangeData;
-    const _currencyLists = Object.keys(_exchangeRates.rates).sort(
-      (a, b) => a - b
-    );
-    console.log("temp currencyList : ", _currencyLists);
-    setExchangeRates(_exchangeRates);
-    setCurrencyLists(_currencyLists);
-  }, []);
 
   useEffect(() => {
     const _fromCurrencyRate = exchangeRates?.rates[fromCurrency];
     const _toCurrencyRate = exchangeRates?.rates[toCurrency];
     console.log(fromCurrencyRate, toCurrencyRate);
     const _amountConvert = (amount / _fromCurrencyRate) * _toCurrencyRate;
-    const _exchangeStatistic = ExchangeStatistic;
-     const _exchangeCart = ExchangeChart;
-    const _chartOption = {
-      xAxis: {
-        type: "category",
-        data: [..._exchangeCart?.batchList[0]?.rates?.slice(1)?.keys()].map(
-          (r) => r - _exchangeCart?.batchList[0]?.rates?.length - 1
-        ),
-        name: "วันที่",
-      },
-      yAxis: {
-        type: "value",
-        name: "อัตราแรกเปลี่ยน",
-        max: "dataMax",
-        min: "dataMin",
-      },
-      series: [
-        {
-          data: _exchangeCart?.batchList[0]?.rates?.slice(1).reverse(),
-          type: "line",
-          smooth: true,
-          lineStyle: { color: "#d5ceeb", width: 5, type: "dashed" },
-        },
-      ],
-      tooltip: {
-        trigger: "axis",
-      },
-    };
 
     setAmountConvert(_amountConvert);
     setFromCurrencyRate(_fromCurrencyRate);
     setToCurrencyRate(_toCurrencyRate);
-    setExchangeRatesStatistic(_exchangeStatistic);
-    setChartOption(_chartOption);
   }, [amount, fromCurrency, toCurrency, exchangeRates]);
+  return {
+    fromCurrency,
+    setFromCurrency,
+    toCurrency,
+    setToCurrency,
+    amount,
+    setAmountConvert,
+    amountConvert,
+    setAmountConvert,
+    fromCurrencyRate,
+    setFromCurrencyRate,
+    toCurrencyRate,
+    setToCurrencyRate,
+  };
+};
+
+const useExchangeStatistic = ({ authToken, fromCurrency, toCurrency }) => {
+  const [exchangeStatistic, setExchangeRatesStatistic] = useState();
+  console.log("useExchangeStatistic");
+  useEffect(() => {
+    axios({
+      url: `https://anyorigin-iinykauowa-uc.a.run.app/?url=${encodeURIComponent(
+        `https://www.xe.com/api/protected/statistics/?from=${fromCurrency}&to=${toCurrency}`
+      )}`,
+      headers: {
+        Authorization: authToken,
+      },
+    }).then((res) => {
+      setExchangeRatesStatistic(res.data);
+    });
+  }, [fromCurrency, toCurrency]);
+  return [exchangeStatistic];
+};
+
+const useChartOption = ({ authToken, fromCurrency, toCurrency }) => {
+  const [chartOption, setChartOption] = useState({});
+  console.log("useChartOption");
+
+  useEffect(() => {
+    axios({
+      url: `https://anyorigin-iinykauowa-uc.a.run.app/?url=${encodeURIComponent(
+        `https://www.xe.com/api/protected/charting-rates/?fromCurrency=${fromCurrency}&toCurrency=${toCurrency}`
+      )}`,
+      headers: {
+        Authorization: authToken,
+      },
+    }).then((res) => {
+      const _exchangeChart = res.data;
+      const _chartOption = {
+        xAxis: {
+          type: "category",
+          data: [..._exchangeChart?.batchList[0]?.rates?.slice(1)?.keys()].map(
+            (r) => r - _exchangeChart?.batchList[0]?.rates?.length - 1
+          ),
+          name: "วันที่",
+        },
+        yAxis: {
+          type: "value",
+          name: "อัตราแรกเปลี่ยน",
+          max: "dataMax",
+          min: "dataMin",
+        },
+        series: [
+          {
+            data: _exchangeChart?.batchList[0]?.rates?.slice(1).reverse(),
+            type: "line",
+            smooth: true,
+            lineStyle: { color: "#83a832", width: 2, type: "default" },
+          },
+        ],
+        tooltip: {
+          trigger: "axis",
+        },
+      };
+
+      setChartOption(_chartOption);
+    });
+  }, [fromCurrency, toCurrency]);
+
+  return [chartOption];
+};
+
+const Exchange = () => {
+  const authToken =
+    "Basic bG9kZXN0YXI6WnoxdndXVmFVRXdFZUFkdkpIWjFuMEY0bXRROWY4U1g=";
+
+  const { exchangeRates, currencyLists } = useFetchExchangeRate({ authToken });
+  const {
+    fromCurrency,
+    setFromCurrency,
+    toCurrency,
+    setToCurrency,
+    amount,
+    setAmount,
+    amountConvert,
+    fromCurrencyRate,
+    toCurrencyRate,
+  } = useConvertExchangeRate({ exchangeRates });
+
+  const [exchangeStatistic] = useExchangeStatistic({
+    authToken,
+    fromCurrency,
+    toCurrency,
+  });
+
+  const [chartOption] = useChartOption({
+    authToken,
+    fromCurrency,
+    toCurrency,
+  });
+
+  const switchCurrency = () => {
+    // USD => THB || THB => USD
+    // TEMP => USD || THB = TEMP(USD)
+
+    const temp = fromCurrency; //ต้นทาง
+    console.log(temp, fromCurrency, toCurrency);
+    setFromCurrency(toCurrency);
+    setToCurrency(temp);
+  };
 
   return (
     <div className="">
       <div className="w-1/2 mx-auto bg-gray-100 mt-8 p-6">
         <h1 className="font-bold text-xl text-center">คำนวนอัตราแลกเปลี่ยน</h1>
-        <form>
+        <div>
           <div className="flex mt-4 space-x-8">
             <div className="w-1/3">
               <label>จำนวน</label>
@@ -81,7 +184,7 @@ const Exchange = () => {
                 name="amount"
                 className="p-2 w-full mt-2"
                 placeholder="1"
-                onChange={(e) => setAmount(Number(e.target.value))}
+                onChange={(e) => setAmount(e.target.value)}
                 value={amount}
               ></input>
             </div>
@@ -91,7 +194,7 @@ const Exchange = () => {
               <select
                 className="p-2 w-full mt-2"
                 value={fromCurrency}
-                onChange={(e) => setFromCurrency(Number(e.target.value))}
+                onChange={(e) => setFromCurrency(e.target.value)}
               >
                 {currencyLists
                   ?.filter((currency) => currency !== toCurrency)
@@ -102,7 +205,7 @@ const Exchange = () => {
                   ))}
               </select>
             </div>
-            <button className="text-lg mt-8">
+            <button className="text-lg mt-8" onClick={switchCurrency}>
               <BsArrowLeftRight />
             </button>
             <div className="w-1/3">
@@ -136,7 +239,7 @@ const Exchange = () => {
               {fromCurrency}
             </div>
           </div>
-        </form>
+        </div>
       </div>
       <div className="w-1/2 mx-auto  mt-8">
         <h2 className="text-lg font-bold">อัตราแลกเปลี่ยนย้อนหลัง</h2>
@@ -192,6 +295,9 @@ const Exchange = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="mt-4">
+        <ReactECharts option={chartOption} />
       </div>
     </div>
   );
