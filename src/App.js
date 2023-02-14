fa50e7a53985bbf90d344f57dbdf6b4fc2204d0c
kunlanis <yoo.kunlanis@gmail.com>
2023-02-14 10:26:43 +0700

    Custom Hook 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import CustomHook1 from "./CustomHook/CustomHook1";
 import CustomHook2 from "./CustomHook/CustomHook2";
+import CustomHook3 from "./CustomHook/CustomHook3";
 import Exchange from "./Exchange/Exchange";
 
 const App = () => {
   return (
     <>
       {/* <CustomHook1 /> */}
-      <CustomHook2 />
+      {/* <CustomHook2 /> */}
+      <CustomHook3 />
     </>
   );
 };
