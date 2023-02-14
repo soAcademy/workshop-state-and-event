f8409cfc26449106bf557719690636d0dbdd7a30
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-14 09:47:16 +0700

    feat: refactoring forget to save custom hook1

--- src/CustomHook/CustomHook1.js
+++ src/CustomHook/CustomHook1.js
@@ -1,41 +1,13 @@
 import React, { useState } from "react";
 
-// refactoring with  custom hook
-const CustomHook1 = () => {
-  const [title, setTitle] = useState("boeing");
-
-  return (
-    <div className="bg-red-200">
-      <p>{title}</p>
-      <p>
-        <button className="p-4 bg-blue-200" onClick={() => setTitle("mike")}>
-          Change Title
-        </button>
-      </p>
-    </div>
-  );
-};
-
-export default CustomHook1;
-
-// import React, { useState } from "react";
-
-// const useTitle = (defaultText) => {
-//   const [title, setTitle] = useState(defaultText);
-//   return {
-//     title,
-//     setTitle,
-//   };
-// };
-
 // const CustomHook1 = () => {
-//   const { title, setTitle } = useTitle("Bin");
+//   const [title, setTitle] = useState("boeing");
 
 //   return (
 //     <div className="bg-red-200">
 //       <p>{title}</p>
 //       <p>
-//         <button className="p-4 bg-blue-200" onClick={() => setTitle("Jam")}>
+//         <button className="p-4 bg-blue-200" onClick={() => setTitle("mike")}>
 //           Change Title
 //         </button>
 //       </p>
@@ -44,3 +16,29 @@ export default CustomHook1;
 // };
 
 // export default CustomHook1;
+
+// refactoring with  custom hook
+const useTitle = (defaultText) => {
+  const [title, setTitle] = useState(defaultText);
+  return {
+    title,
+    setTitle,
+  };
+};
+
+const CustomHook1 = () => {
+  const { title, setTitle } = useTitle("Bin");
+
+  return (
+    <div className="bg-red-200">
+      <p>{title}</p>
+      <p>
+        <button className="p-4 bg-blue-200" onClick={() => setTitle("Jam")}>
+          Change Title
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default CustomHook1;
