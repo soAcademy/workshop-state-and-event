98dcb76647598b6daa607c4c037b4dadd9cd16fc
jaiieth <midzty.rb128@gmail.com>
2023-02-14 10:26:22 +0700

    feat: add CustomHook4

---
+++ src/CustomHook/CustomHook4.js
@@ -0,0 +1,24 @@
+import { useFetch } from "./useCustomHook";
+import { useData } from "./useCustomHook";
+const CustomHook4 = () => {
+  const {data, setData} = useData();
+  const {isFetch, setIsFetch} = useFetch({
+    method: "get",
+    url: "https://api.sampleapis.com/coffee/hot",
+    setData: setData,
+  });
+  return (
+    <>
+      <p>
+        <button
+          className="w-24 bg-red-300 p-4"
+          onClick={() => setIsFetch(!isFetch)}
+        >
+          Fetch
+        </button>
+      </p>
+      <p>{isFetch && JSON.stringify(data)}</p>
+    </>
+  );
+};
+export default CustomHook4;
