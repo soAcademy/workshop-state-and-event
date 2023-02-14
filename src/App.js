9b09216d0301abc716b3d92d459fe055b47c2b40
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-14 10:40:30 +0700

    feat: refactoring hook 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import CustomHook1 from "./CustomHook/CustomHook1";
 import CustomHook2 from "./CustomHook/CustomHook2";
 import CustomHook3 from "./CustomHook/CustomHook3";
+import CustomHook4 from "./CustomHook/CustomHook4";
 
 const App = () => {
   return (
     <>
-      <CustomHook3 />
+      <CustomHook4 />
     </>
   );
 };
