ad12d815b830207c42bfac04c7ee33c8af076cb8
Panasun Sunanta <panasunsunanta@gmail.com>
2023-02-14 08:54:46 +0700

    fix: customHook

--- src/CustomHookAnswer/CustomHook3.js
+++ src/CustomHookAnswer/CustomHook3.js
@@ -1,49 +1,12 @@
-// import React, { useState, useEffect } from "react";
-
-// const CustomHook3 = () => {
-//   const [num, setNum] = useState(0);
-//   const [numSquare, setNumSquare] = useState(0);
-
-//   useEffect(() => {
-//     setNumSquare(Number(num) ** 2);
-//   }, [num]);
-
-//   return (
-//     <>
-//       <p>Num: {num}</p>
-//       <p>Num Square: {numSquare}</p>
-//       <input
-//         className="border border-black bg-yellow-200 w-64"
-//         onChange={(e) => setNum(e.target.value)}
-//       />
-//     </>
-//   );
-// };
-
-// export default CustomHook3;
-
 import React, { useState, useEffect } from "react";
 
-const useNum = () => {
+const CustomHook3 = () => {
   const [num, setNum] = useState(0);
-  return {
-    num,
-    setNum,
-  };
-};
-
-const useNumSquare = ({ num }) => {
   const [numSquare, setNumSquare] = useState(0);
 
   useEffect(() => {
     setNumSquare(Number(num) ** 2);
   }, [num]);
-  return { numSquare, setNumSquare };
-};
-
-const CustomHook3 = () => {
-  const { num, setNum } = useNum();
-  const { numSquare } = useNumSquare({ num });
 
   return (
     <>
@@ -58,3 +21,40 @@ const CustomHook3 = () => {
 };
 
 export default CustomHook3;
+
+// import React, { useState, useEffect } from "react";
+
+// const useNum = () => {
+//   const [num, setNum] = useState(0);
+//   return {
+//     num,
+//     setNum,
+//   };
+// };
+
+// const useNumSquare = ({ num }) => {
+//   const [numSquare, setNumSquare] = useState(0);
+
+//   useEffect(() => {
+//     setNumSquare(Number(num) ** 2);
+//   }, [num]);
+//   return { numSquare, setNumSquare };
+// };
+
+// const CustomHook3 = () => {
+//   const { num, setNum } = useNum();
+//   const { numSquare } = useNumSquare({ num });
+
+//   return (
+//     <>
+//       <p>Num: {num}</p>
+//       <p>Num Square: {numSquare}</p>
+//       <input
+//         className="border border-black bg-yellow-200 w-64"
+//         onChange={(e) => setNum(e.target.value)}
+//       />
+//     </>
+//   );
+// };
+
+// export default CustomHook3;
