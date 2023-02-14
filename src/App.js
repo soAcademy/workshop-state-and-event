9e602b4c71d0048d1c9ae1bffbbb88d38355d094
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-14 10:21:23 +0700

    feat: customhook 3 refactoring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import CustomHook1 from "./CustomHook/CustomHook1";
 import CustomHook2 from "./CustomHook/CustomHook2";
+import CustomHook3 from "./CustomHook/CustomHook3";
 
 const App = () => {
   return (
     <>
-      <CustomHook2 />
+      <CustomHook3 />
     </>
   );
 };
--- src/CustomHook/CustomHook3.js
+++ src/CustomHook/CustomHook3.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const useNumber = () => {
+  const [num, setNum] = useState(0);
+  return {
+    num,
+    setNum,
+  };
+};
+
+// const min1 = (x, y) => x < y ? x : y;
+// const min2 = (args) => args.x < args.y ? args.y : args.x;
+
+// min1(3, 5, 10, 5, 12);
+// min2({x: 3, y: 5})
+// min2({y: 5, x: 3, a1: 5, a2: 10, a3: 1})
+
+const useNumSquare = (num) => {
+  const [numSquare, setNumSquare] = useState(0);
+
+  useEffect(() => {
+    setNumSquare(Number(num) ** 2);
+  }, [num]);
+
+  return { numSquare, setNumSquare };
+};
 
 const CustomHook3 = () => {
-  return <div>CustomHook3</div>;
+  // เรียกใช้งาน
+  const { num, setNum } = useNumber();
+  const { numSquare, setNumSquare } = useNumSquare(num);
+
+  return (
+    <>
+      <p>Num: {num}</p>
+      <p>Num Square: {numSquare}</p>
+      <input
+        className="border border-black bg-yellow-200 w-64"
+        onChange={(e) => setNum(e.target.value)}
+      />
+    </>
+  );
 };
 
 export default CustomHook3;
