ace08baa152920cc58243d39be2c6ead40f681b0
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-02-14 11:31:18 +0700

    feat: separate custom hook to other file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import CustomHook1 from "./CustomHook/CustomHook1";
 import CustomHook2 from "./CustomHook/CustomHook2";
 import CustomHook3 from "./CustomHook/CustomHook3";
 import CustomHook4 from "./CustomHook/CustomHook4";
+import CurrencyConverter7 from "./CurrencyConverter/CurrencyConverter7";
 
 const App = () => {
   return (
     <>
-      <CustomHook4 />
+      {/* <CustomHook4 /> */}
+      <CurrencyConverter7 />
     </>
   );
 };
