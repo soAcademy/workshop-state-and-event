4a03a36c836d4e0a3290bb37b65dfec8357fb371
Firstweera <firstweera357@gmail.com>
2023-02-14 09:29:40 +0700

    feat: add app8

---
+++ src/App8.js
@@ -0,0 +1,122 @@
+// import State3 from "./StatesAnswer/State3";
+// import LocalStorage1 from "./LocalStorageAnswer/LocalStorage1";
+// import LocalStorage2 from "./LocalStorageAnswer/LocalStorage2";
+// import LocalStorage3 from "./LocalStorageAnswer/LocalStorage3";
+// import LocalStorage4 from "./LocalStorageAnswer/LocalStorage4";
+// import ToDoList1 from "./ToDoListAnswers/ToDoList1";
+// import ToDoList2 from "./ToDoListAnswers/ToDoList2";
+// import ToDoList3 from "./ToDoListAnswers/ToDoList3";
+// import ToDoList4 from "./ToDoListAnswers/ToDoList4";
+// import ToDoList5 from "./ToDoListAnswers/ToDoList5";
+// import Chart1 from "./ChartsAnswer/Chart1";
+// import Chart2 from "./ChartsAnswer/Chart2";
+// import Chart3 from "./ChartsAnswer/Chart3";
+// import Accordian1 from "./AccordiansAnswer/Accordian1";
+// import Accordian2 from "./AccordiansAnswer/Accordian2";
+// import Accordian3 from "./AccordiansAnswer/Accordian3";
+// import Trivia1 from "./TriviaAnswer/Trivia1";
+// import Trivia2 from "./TriviaAnswer/Trivia2";
+// import Trivia3 from "./TriviaAnswer/Trivia3";
+// import Trivia4 from "./TriviaAnswer/Trivia4";
+// import Trivia5 from "./TriviaAnswer/Trivia5";
+// import Zipcode1 from "./ZipcodeAnswer/Zipcode1";
+// import Zipcode2 from "./ZipcodeAnswer/Zipcode2";
+// import Zipcode3 from "./ZipcodeAnswer/Zipcode3";
+// import Zipcode4 from "./ZipcodeAnswer/Zipcode4";
+// import Zipcode5 from "./ZipcodeAnswer/Zipcode5";
+// import Zipcode6 from "./ZipcodeAnswer/Zipcode6";
+// import Zipcode7 from "./ZipcodeAnswer/Zipcode7";
+// import Zipcode8 from "./ZipcodeAnswer/Zipcode8";
+// import DeathCause1 from "./DeathCauseAnswer/DeathCause1";
+// import DeathCause2 from "./DeathCauseAnswer/DeathCause2";
+// import DeathCause3 from "./DeathCauseAnswer/DeathCause3";
+// import DeathCause4 from "./DeathCauseAnswer/DeathCause4";
+// import DeathCause5 from "./DeathCauseAnswer/DeathCause5";
+// import DeathCause6 from "./DeathCauseAnswer/DeathCause6";
+// import DeathCause7 from "./DeathCauseAnswer/DeathCause7";
+// import DeathCause8 from "./DeathCauseAnswer/DeathCause8";
+// import Retirement1 from "./RetirementAnswer/Retirement1";
+// import Retirement2 from "./RetirementAnswer/Retirement2";
+// import Retirement3 from "./RetirementAnswer/Retirement3";
+// import Retirement4 from "./RetirementAnswer/Retirement4";
+// import Retirement5 from "./RetirementAnswer/Retirement5";
+// import CurrencyConverter1 from "./CurrencyConverterAnswer/CurrencyConverter1";
+// import Zipcode8 from "./ZipcodeAnswer/Zipcode8";
+// import DeathCause1 from "./DeathCauseAnswer/DeathCause1";
+// import DeathCause2 from "./DeathCauseAnswer/DeathCause2";
+// import DeathCause3 from "./DeathCauseAnswer/DeathCause3";
+// import DeathCause4 from "./DeathCauseAnswer/DeathCause4";
+// import DeathCause8 from "./DeathCauseAnswer/DeathCause8";
+// import DeathCause1 from "./DeathCause/DeathCause1";
+// import DeathCause2 from "./DeathCause/DeathCause2";
+// import DeathCause3 from "./DeathCause/DeathCause3";
+// import DeathCause4 from "./DeathCause/DeathCause4";
+// import DeathCause5 from "./DeathCause/DeathCause5";
+// import DeathCause6 from "./DeathCause/DeathCause6";
+// import DeathCause7 from "./DeathCause/DeathCause7";
+// import DeathCause8 from "./DeathCause/DeathCause8";
+// import Retirement1 from "./Retirement/Retirement1";
+// import Retirement2 from "./Retirement/Retirement2";
+// import CurrencyConverter1 from "./CurrencyConverter/CurrencyConverter1";
+// import CurrencyConverter2 from "./CurrencyConverter/CurrencyConverter2";
+// import CurrencyConverter3 from "./CurrencyConverter/CurrencyConverter3";
+// import CurrencyConverter4 from "./CurrencyConverter/CurrencyConverter4";
+// import CurrencyConverter5 from "./CurrencyConverter/CurrencyConverter5";
+import CurrencyConverter6 from "./CurrencyConverter/CurrencyConverter6";
+
+const App = () => {
+  return (
+    <>
+      {/* <State3 /> */}
+      {/* <LocalStorage1 /> */}
+      {/* <LocalStorage2 /> */}
+      {/* <LocalStorage3 data1="bin" data2="hello"/> */}
+      {/* <LocalStorage4 /> */}
+      {/* <ToDoList1 /> */}
+      {/* <ToDoList2 /> */}
+      {/* <ToDoList3 /> */}
+      {/* <ToDoList4 /> */}
+      {/* <ToDoList5 /> */}
+      {/* <Chart1 /> */}
+      {/* <Chart2 /> */}
+      {/* <Chart3 /> */}
+      {/* <Accordian1 /> */}
+      {/* <Accordian2 /> */}
+      {/* <Accordian3 /> */}
+      {/* <Trivia1 /> */}
+      {/* <Trivia2 /> */}
+      {/* <Trivia3 /> */}
+      {/* <Trivia4 /> */}
+      {/* <Trivia5 /> */}
+      {/* <Zipcode1 /> */}
+      {/* <Zipcode2 /> */}
+      {/* <Zipcode3 /> */}
+      {/* <Zipcode4 /> */}
+      {/* <Zipcode5 /> */}
+      {/* <Zipcode6 /> */}
+      {/* <Zipcode7 /> */}
+      {/* <Zipcode8 /> */}
+      {/* <DeathCause1 /> */}
+      {/* <DeathCause2 /> */}
+      {/* <DeathCause3 /> */}
+      {/* <DeathCause4 /> */}
+      {/* <DeathCause5 /> */}
+      {/* <DeathCause6 /> */}
+      {/* <DeathCause7 /> */}
+      {/* <DeathCause8 /> */}
+      {/* <Retirement1 /> */}
+      {/* <Retirement2 /> */}
+      {/* <Retirement3 /> */}
+      {/* <Retirement4 /> */}
+      {/* <Retirement5 /> */}
+      {/* <CurrencyConverter1 /> */}
+      {/* <CurrencyConverter2 /> */}
+      {/* <CurrencyConverter3 /> */}
+      {/* <CurrencyConverter4 /> */}
+      {/* <CurrencyConverter5 /> */}
+      <CurrencyConverter6 />
+    </>
+  );
+};
+
+export default App;
