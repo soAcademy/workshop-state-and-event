a8a8acb1820e2d0de1ef68c4680f25f9b171c68e
Phisit Siprasatthong <phisite@gmail.com>
2023-03-15 11:15:48 +0700

    feat: apply mui to twitter user profile page

--- src/Twitter/Pages/User.js
+++ src/Twitter/Pages/User.js
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const User = () => {
   const { id } = useParams();
@@ -16,18 +29,54 @@ const User = () => {
   }, [id]);
 
   return (
-    <>
-      <h1>User ID: {id}</h1>
-      <h2>Name: {userProfile?.name}</h2>
-      <ul>
-        {userProfile?.tweets.map((tweet) => (
-          <li>
-            <p>{tweet.tweetText}</p>
-            <small>{tweet.createdAt}</small>
-          </li>
-        ))}
-      </ul>
-    </>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          width: "100%",
+          bgcolor: "background.paper",
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar
+          alt={userProfile?.name}
+          sx={{ m: 1, bgcolor: "secondary.main", width: 64, height: 64 }}
+        />
+        <Typography component="h1" variant="h5">
+          {userProfile?.name}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          {userProfile?.bio}
+        </Typography>
+        <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+          {userProfile?.tweets.map((tweet, idx) => (
+            <>
+              <ListItem alignItems="flex-start" key={tweet.id}>
+                <ListItemAvatar>
+                  <Avatar
+                    alt={userProfile?.name}
+                    // src="/static/images/avatar/1.jpg"
+                  />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={tweet.tweetText}
+                  secondary={new Intl.DateTimeFormat("en-US", {
+                    dateStyle: "full",
+                    timeStyle: "long",
+                    timeZone: "Asia/Bangkok",
+                  }).format(Date.parse(tweet.createdAt))}
+                />
+              </ListItem>
+              {idx < userProfile.tweets.length - 1 && (
+                <Divider component="li" />
+              )}
+            </>
+          ))}
+        </List>
+      </Box>
+    </Container>
   );
 };
 
